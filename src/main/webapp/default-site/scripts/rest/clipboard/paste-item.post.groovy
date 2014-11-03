16b4c319d2ce7adb26f9da828e52153049de7467
Hyanghee Lim <hlim@rivetlogic.com>
2014-11-03 18:14:18 -0500

    updated paste service call

--- src/main/webapp/default-site/scripts/rest/clipboard/paste-item.post.groovy
+++ src/main/webapp/default-site/scripts/rest/clipboard/paste-item.post.groovy
@@ -9,11 +9,11 @@ def requestBody = request.reader.text
 
 def serverProperties = applicationContext.get("studio.crafter.properties")
 def alfrescoUrl = serverProperties["alfrescoUrl"] // http://127.0.0.1:8080/alfresco
-//def ticket = Cookies.getCookieValue("ccticket", request)
-def pasteServiceUrl = "/cstudio/wcm/clipboard/paste"
+def ticket = Cookies.getCookieValue("ccticket", request)
+def pasteServiceUrl = "/service/cstudio/wcm/clipboard/paste"
+def serviceUrl = alfrescoUrl + pasteServiceUrl;
 
-Clipboard.paste(site, session, destination, alfrescoUrl + pasteServiceUrl, "test")
-
+result.status = Clipboard.paste(site, session, destination, serviceUrl, ticket)
 result.site = site
 
 return result
