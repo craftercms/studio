d7f67e6107875998488864a555b748188d6c4a48
Russ Danner <rdanner@rivetlogic.com>
2014-11-20 23:47:33 -0500

    cast integer parameters

--- src/main/webapp/default-site/scripts/rest/api/1/deployment/get-deployment-history.get.groovy
+++ src/main/webapp/default-site/scripts/rest/api/1/deployment/get-deployment-history.get.groovy
@@ -2,8 +2,8 @@ import scripts.api.DeploymentServices;
 
 def result = [:]
 def site = params.site
-def days = params.days
-def num = params.num
+def days = params.days.toInteger()
+def num = params.num.toInteger()
 def filterType = params.filterType
 
 def context = DeploymentServices.createContext(applicationContext, request)
