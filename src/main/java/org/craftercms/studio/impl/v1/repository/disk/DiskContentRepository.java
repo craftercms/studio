d20d44124616b3ff2ac57d1968efe569ca0ab6bf
Russ Danner <rdanner@rivetlogic.com>
2015-05-27 19:24:49 -0400

    http://issues.craftercms.org/browse/CRAFTERCMS-1047

--- src/main/java/org/craftercms/studio/impl/v1/repository/disk/DiskContentRepository.java
+++ src/main/java/org/craftercms/studio/impl/v1/repository/disk/DiskContentRepository.java
@@ -286,9 +286,10 @@ public class DiskContentRepository extends AbstractContentRepository implements 
 
         synchronized(path) {
             versionId = determineNextVersionLabel(path, majorVersion);
+            InputStream content = null;
 
             try {
-                InputStream content = getContent(path);
+                content = getContent(path);
                 String versionPath = path+"--"+versionId;
 
                 CopyOption options[] = { StandardCopyOption.REPLACE_EXISTING };
@@ -301,6 +302,9 @@ public class DiskContentRepository extends AbstractContentRepository implements 
             catch(Exception err) {
                 logger.error("error versionign file: "+path, err);
                 versionId = null;
+            }
+            finally {
+                closeInputStreamQuietly(content);
             }
         }
 
@@ -317,9 +321,11 @@ public class DiskContentRepository extends AbstractContentRepository implements 
 
         synchronized(path) {
             String versionId = determineNextVersionLabel(path, major);
-
+            InputStream versionContent = null;
+            InputStream wipContent = null;
+            
             try {
-                InputStream versionContent = getVersionedContent(path, label);
+                versionContent = getVersionedContent(path, label);
                 String versionPath = path+"--"+versionId;
 
                 CopyOption options[] = { StandardCopyOption.REPLACE_EXISTING };
@@ -328,13 +334,18 @@ public class DiskContentRepository extends AbstractContentRepository implements 
                 Files.copy(versionContent, constructVersionRepoPath(versionPath), options);
 
                 // write the repo content
-                InputStream wipContent = getVersionedContent(path, label);
+                wipContent = getVersionedContent(path, label);
                 Files.copy(wipContent, constructRepoPath(path), options);
             }
             catch(Exception err) {
                 logger.error("error versionign file: "+path, err);
                 versionId = null;
             }
+            finally {
+                closeInputStreamQuietly(versionContent);
+                closeInputStreamQuietly(wipContent);
+            }
+
         }
 
         return success;
@@ -458,7 +469,25 @@ public class DiskContentRepository extends AbstractContentRepository implements 
 
     }
 
+    protected boolean closeInputStreamQuietly(InputStream is) {
+        boolean success = true;
 
+        if(is != null) { 
+            try { 
+                is.close(); 
+            } 
+            catch(Exception ioErr) { 
+                success = false;
+
+                /* eat error */
+                if(Logger.LEVEL_DEBUG.equals(logger.getLevel())) {
+                    logger.error("Error while closing InputStream quietly", ioErr);
+                } 
+            } 
+        } 
+
+        return success;     
+    }
 
     public Reactor getRepositoryReactor() { return repositoryReactor; }
     public void setRepositoryReactor(Reactor repositoryReactor) { this.repositoryReactor = repositoryReactor; }
