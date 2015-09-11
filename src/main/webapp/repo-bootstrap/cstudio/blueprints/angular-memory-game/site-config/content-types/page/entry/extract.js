1c4611562f26f14e14ffdfc4dfe2d9e1eba6e002
Russ Danner <rdanner@rivetlogic.com>
2015-09-11 09:35:10 -0400

    updating blueprints

--- src/main/webapp/repo-bootstrap/cstudio/blueprints/angular-memory-game/site-config/content-types/page/entry/extract.js
+++
@@ -1,9 +0,0 @@
-<import resource="classpath:alfresco/templates/webscripts/org/rivetlogic/cstudio/common/lib/common-extraction-api.js">
-
-contentNode.addAspect("cstudio-core:pageMetadata");
-
-var root = contentXml.getRootElement();
-
-extractCommonProperties(contentNode, root);
-
-contentNode.save();
