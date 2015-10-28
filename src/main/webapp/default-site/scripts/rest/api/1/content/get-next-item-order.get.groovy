6c5be80546c1f7540bf73767d61e9908dddae7ad
Dejan Brkic <dejan.brkic@craftersoftware.com>
2015-10-28 16:40:03 -0600

    CRAFTERCMS-1581 fixed

--- src/main/webapp/default-site/scripts/rest/api/1/content/get-next-item-order.get.groovy
+++ src/main/webapp/default-site/scripts/rest/api/1/content/get-next-item-order.get.groovy
@@ -2,7 +2,7 @@ import scripts.api.PageNavigationOrderServices;
 
 def result = [:]
 def site = params.site
-def path = params.path
+def path = params.parentpath
 
 def context = PageNavigationOrderServices.createContext(applicationContext, request)
 result.nextValue = PageNavigationOrderServices.getNextItemOrder(context, site, path);
