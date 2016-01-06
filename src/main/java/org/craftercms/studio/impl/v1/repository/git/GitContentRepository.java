b3c19fa1762d2efb50cf8d72f4711fc38abd76a4
Dejan Brkic <dejan.brkic@craftersoftware.com>
2016-01-06 08:40:41 -0600

    git repo work in progress

--- src/main/java/org/craftercms/studio/impl/v1/repository/git/GitContentRepository.java
+++ src/main/java/org/craftercms/studio/impl/v1/repository/git/GitContentRepository.java
@@ -20,8 +20,16 @@
 package org.craftercms.studio.impl.v1.repository.git;
 
 import org.craftercms.studio.impl.v1.repository.disk.DiskContentRepository;
+import org.eclipse.jgit.errors.AmbiguousObjectException;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.internal.storage.file.LockFile;
+import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.treewalk.FileTreeIterator;
 import org.eclipse.jgit.treewalk.TreeWalk;
@@ -45,26 +53,50 @@ public class GitContentRepository extends DiskContentRepository {
     public void lockItem(String site, String path) {
         try {
             Repository repo = new FileRepositoryBuilder()
-                    .setGitDir(new File(this.getRootPath()))
+                    .setGitDir(new File(this.getRootPath() + "/wem-projects/" + site + "/" + site + "/centralrepo"))
+                    .readEnvironment()
+                    .findGitDir()
                     .build();
-            TreeWalk tw = new TreeWalk(repo);
-            ObjectId tree = repo.resolve("HEAD^{tree}");
-            tw.addTree(tree); // tree ‘0’
-            tw.setRecursive(true);
-            tw.setFilter(PathFilter.create(path));
-            FileTreeIterator iterator = tw.getTree(0, FileTreeIterator.class);
-            iterator.getEntryFile()
-            while(tw.next()) {
-                ObjectId id = tw.getObjectId(0);
-                tw.getPathString();
-                repo.getFS();
-            }
-            tw.close();
-            FS fs = FS.detect();
 
+            TreeWalk tw = new TreeWalk(repo);
+
+            RevTree tree = getTree(repo);
+            tw.addTree(tree); // tree ‘0’
+            tw.setRecursive(false);
+            tw.setFilter(PathFilter.create(path));
+
+            if (!tw.next()) {
+                return;
+            }
+
+            FS fs = FS.detect();
+            ObjectId id = tw.getObjectId(0);
+            File file = new File(tw.getPathString());
+            LockFile lock = new LockFile(file, fs);
+            lock.lock();
+
+            tw.close();
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private RevTree getTree(Repository repository) throws AmbiguousObjectException, IncorrectObjectTypeException,
+            IOException, MissingObjectException {
+        ObjectId lastCommitId = repository.resolve(Constants.HEAD);
+
+
+        // a RevWalk allows to walk over commits based on some filtering
+        try (RevWalk revWalk = new RevWalk(repository)) {
+            RevCommit commit = revWalk.parseCommit(lastCommitId);
+
+            System.out.println("Time of commit (seconds since epoch): " + commit.getCommitTime());
+
+            // and using commit's tree find the path
+            RevTree tree = commit.getTree();
+            System.out.println("Having tree: " + tree);
+            return tree;
         }
     }
 }
