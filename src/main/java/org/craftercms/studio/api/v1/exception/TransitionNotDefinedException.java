dfd9c26be25f18150309cd5ebec3104d962b0376
Russ Danner <rdanner@rivetlogic.com>
2016-02-01 14:00:24 -0500

    replace 2013 in copyright with 2016

--- src/main/java/org/craftercms/studio/api/v1/exception/TransitionNotDefinedException.java
+++ src/main/java/org/craftercms/studio/api/v1/exception/TransitionNotDefinedException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2013 Crafter Software Corporation.
+ * Copyright (C) 2007-2016 Crafter Software Corporation.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
