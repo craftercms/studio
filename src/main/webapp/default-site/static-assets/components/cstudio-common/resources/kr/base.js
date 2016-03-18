4abb6e8962fc241fbb0fe2bfa08db04657abec37
Hyanghee Lim <hlim@rivetlogic.com>
2016-03-18 10:40:21 -0400

    CRAFTERCMS-1862: Added a browsePath property to Child Content datasource

--- src/main/webapp/default-site/static-assets/components/cstudio-common/resources/kr/base.js
+++ src/main/webapp/default-site/static-assets/components/cstudio-common/resources/kr/base.js
@@ -460,6 +460,7 @@ CStudioAuthoring.Messages.registerBundle("contentTypes", "kr", {
     dataType: "데이터 형식",
     listName: "목록 이름",
     repositoryPath: "저장소 경로",
+    browsePath: "찾기 경로",
     string: "현",
     integer: "정수",
     float: "플로트",
