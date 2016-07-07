ee451fa9595abeeb70bd436e1c8197fd9535537b
Carlos Ortiz <carlos.ortiz@craftercms.org>
2016-07-07 08:39:25 -0600

    remove static-assets

--- src/main/webapp/default-site/static-assets/jquery/jquery-time/jquery.timeentry-de-min.js
+++
@@ -1,3 +0,0 @@
-(function(a){a.timeEntry.regional.de={show24Hours:true,separator:":",ampmPrefix:"",ampmNames:["AM","PM"],spinnerTexts:["Jetzt","vorheriges Feld","nächstes Feld","hoch","runter"]};
-a.timeEntry.setDefaults(a.timeEntry.regional.de)
-})(jQuery);
--- src/main/webapp/default-site/static-assets/jquery/jquery-time/jquery.timeentry-fr-min.js
+++
@@ -1,3 +0,0 @@
-(function(a){a.timeEntry.regional.fr={show24Hours:true,separator:":",ampmPrefix:"",ampmNames:["AM","PM"],spinnerTexts:["Maintenant","Précédent","Suivant","Augmentez","Amoindrissez"]};
-a.timeEntry.setDefaults(a.timeEntry.regional.fr)
-})(jQuery);
