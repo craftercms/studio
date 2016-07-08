e804337bc15cec2eb0b4fdab4204e940fd3bdaad
Carlos Ortiz <carlos.ortiz@craftercms.org>
2016-07-08 13:47:39 -0600

    Move Static Assets to https://github.com/craftercms/studio2-ui

--- src/main/webapp/default-site/static-assets/jquery/jquery-time/jquery.timeentry-is-min.js
+++
@@ -1,3 +0,0 @@
-(function(a){a.timeEntry.regional.is={show24Hours:true,separator:":",ampmPrefix:"",ampmNames:["fh","eh"],spinnerTexts:["Núna","Fyrra svæði","Næsta svæði","Hækka","Lækka"]};
-a.timeEntry.setDefaults(a.timeEntry.regional.is)
-})(jQuery);
