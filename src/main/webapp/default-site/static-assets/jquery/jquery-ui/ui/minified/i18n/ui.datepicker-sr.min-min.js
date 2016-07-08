d20426d5851f26016deaef85b78b34150ba58975
jvega-crafter <jose.vega@craftersoftware.com>
2016-07-08 14:44:09 -0600

    Deleted static-assets folders

--- src/main/webapp/default-site/static-assets/jquery/jquery-ui/ui/minified/i18n/ui.datepicker-sr.min-min.js
+++
@@ -1,3 +0,0 @@
-jQuery(function(b){b.datepicker.regional.sr={closeText:"Затвори",prevText:"&#x3c;",nextText:"&#x3e;",currentText:"Данас",monthNames:["Јануар","Фебруар","Март","Април","Мај","Јун","Јул","Август","Септембар","Октобар","Новембар","Децембар"],monthNamesShort:["Јан","Феб","Мар","Апр","Мај","Јун","Јул","Авг","Сеп","Окт","Нов","Дец"],dayNames:["Недеља","Понедељак","Уторак","Среда","Четвртак","Петак","Субота"],dayNamesShort:["Нед","Пон","Уто","Сре","Чет","Пет","Суб"],dayNamesMin:["Не","По","Ут","Ср","Че","Пе","Су"],dateFormat:"dd/mm/yy",firstDay:1,isRTL:false};
-b.datepicker.setDefaults(b.datepicker.regional.sr)
-});
