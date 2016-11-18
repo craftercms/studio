5b0393fe9649e9483a2eda3fddd4078320b362fa
Carlos Ortiz <carlos.ortiz@craftercms.org>
2016-11-18 08:08:29 -0600

    Remove Extra println

--- src/main/webapp/default-site/scripts/rest/api/1/content/get-items-tree.get.groovy
+++ src/main/webapp/default-site/scripts/rest/api/1/content/get-items-tree.get.groovy
@@ -9,12 +9,9 @@ def depth = params.depth ? params.depth.toInteger() : 666;
  * After 11/15/16 depth was ignore and internal was
  * hardcoded to 2, this was fix ,but UI expect that min possible value is 2
  * **/
-println depth
 if(depth<=1){
     depth=2
 }
-println depth
-
 def context = ContentServices.createContext(applicationContext, request)
 
 result.item = ContentServices.getContentItemTree(site, path, depth, context)
