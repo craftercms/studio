6ac85078f6be314d8c775eabd79ec0c76fc6b1ec
Sumer Jabri <sjabri@rivetlogic.com>
2016-12-06 18:36:45 -0500

    Update the configuration management system to allow overrides. Added base config for git.

--- src/main/java/org/craftercms/studio/api/v1/util/StudioConfiguration.java
+++ src/main/java/org/craftercms/studio/api/v1/util/StudioConfiguration.java
@@ -21,11 +21,12 @@ package org.craftercms.studio.api.v1.util;
 
 public interface StudioConfiguration {
     public static final String LOAD_ADDITIONAL_CONFIGURATION = "load-additional-configuration";
-    public static final String GIT_REPO_BASE = "git-repo-base";
+    public static final String REPO_BASE_PATH = "repo-base-path";
     public static final String GLOBAL_REPO_PATH = "global-repo-path";
-    public static final String SITES_REPO_PATH = "sites-repo-path";
+    public static final String SITES_REPOS_PATH = "sites-repos-path";
     public static final String SANDBOX_PATH = "sandbox-path";
     public static final String PUBLISHED_PATH = "published-path";
+    public static final String BLUE_PRINTS_PATH = "blue-prints-path";
 
     // Methods
     void loadConfiguration();
