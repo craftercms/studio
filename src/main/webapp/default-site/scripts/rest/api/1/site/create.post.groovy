8dfea0bba3d32a1529b1ea42e20677d041063b84
Dejan Brkic <dejan.brkic@craftersoftware.com>
2017-03-21 22:48:53 +0100

    CRAFTERCMS-625 implemented new site apis

---
+++ src/main/webapp/default-site/scripts/rest/api/1/site/create.post.groovy
@@ -0,0 +1,49 @@
+/*
+ * Crafter Studio Web-content authoring solution
+ * Copyright (C) 2007-2017 Crafter Software Corporation.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+import org.craftercms.studio.api.v1.exception.SiteAlreadyExistsException
+import scripts.api.SiteServices;
+import groovy.json.JsonSlurper
+
+def result = [:]
+def requestJson = request.reader.text
+def slurper = new JsonSlurper()
+def parsedReq = slurper.parseText(requestJson)
+
+def blueprint = parsedReq.blueprint
+def siteId = parsedReq.site_id
+def description = parsedReq.description
+
+def context = SiteServices.createContext(applicationContext, request)
+try {
+    SiteServices.createSiteFromBlueprint(context, blueprint, siteId, siteId, description)
+    result.message = "OK"
+    def locationHeader = request.getRequestURL().toString().replace(request.getPathInfo().toString(), "") + "/api/1/services/api/1/site/get.json?site_id=" + siteId
+    response.addHeader("Location", locationHeader)
+    response.setStatus(201)
+} catch (SiteAlreadyExistsException e) {
+    response.setStatus(409)
+    def locationHeader = request.getRequestURL().toString().replace(request.getPathInfo().toString(), "") + "/api/1/services/api/1/site/get.json?site_id=" + siteId
+    response.addHeader("Location", locationHeader)
+    result.message = "Site already exists"
+} catch (Exception e) {
+    response.setStatus(500)
+    result.message = "Internal server error: \n" + e
+}
+return result
