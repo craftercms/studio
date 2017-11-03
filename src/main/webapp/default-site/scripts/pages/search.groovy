40d78b8cfd3afb41f56b223a03f90d53d3780668
Alfonso Vasquez <alfonso.vasquez@craftersoftware.com>
2017-11-03 12:33:11 -0400

    Validation enhancements:
    * Search page code now validates that the keyword terms.

--- src/main/webapp/default-site/scripts/pages/search.groovy
+++ src/main/webapp/default-site/scripts/pages/search.groovy
@@ -1,7 +1,18 @@
+import org.craftercms.commons.validation.validators.impl.NoTagsValidator
+import org.craftercms.engine.exception.HttpStatusCodeException
+import org.springframework.http.HttpStatus
+
 import scripts.libs.EnvironmentOverrides
+import scripts.libs.utils.ValidationUtils
 
 model.envConfig = EnvironmentOverrides.getValuesForSite(applicationContext, request, response)
 model.cookieDomain = request.getServerName()
 model.keywordTerm = (params.s) ? params.s : ""
 model.mode = (params.mode) ? params.mode : "select"
 model.context = (params.context) ? params.context : "default"
+
+try {
+    ValidationUtils.validateInput(model.keywordTerm, new NoTagsValidator("keywordTerm"))
+} catch (e) {
+    throw new HttpStatusCodeException(HttpStatus.BAD_REQUEST, "Invalid keywordTerm input", e)
+}
