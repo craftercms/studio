4cbebc366060cbdaa30dd19e03b6ce457c861d2a
Dejan Brkic <dejan.brkic@craftersoftware.com>
2018-01-04 23:21:24 +0100

    CRAFTERCMS-1740 Fixed bug issue with authentication headers and first page load of studio

--- src/main/webapp/default-site/scripts/pages/entry.groovy
+++ src/main/webapp/default-site/scripts/pages/entry.groovy
@@ -1,9 +1,23 @@
+import org.apache.commons.lang3.StringUtils
 import scripts.api.SecurityServices
+
+import static org.craftercms.studio.api.v1.util.StudioConfiguration.AUTHENTICATION_HEADERS_ENABLED
+import static org.craftercms.studio.api.v1.util.StudioConfiguration.AUTHENTICATION_HEADERS_USERNAME
 
 def context = SecurityServices.createContext(applicationContext, request)
 
 def result = [:]
 def currentUser = SecurityServices.getCurrentUser(context)
+
+if (StringUtils.isEmpty(currentUser)) {
+    def studioConfigurationSB = context.applicationContext.get("studioConfiguration")
+    def enabledString = studioConfigurationSB.getProperty(AUTHENTICATION_HEADERS_ENABLED)
+    def enabled = Boolean.parseBoolean(enabledString)
+    if (enabled) {
+        currentUser = request.getHeader(studioConfigurationSB.getProperty(AUTHENTICATION_HEADERS_USERNAME))
+    }
+}
+
 def profile = SecurityServices.getUserProfile(context, currentUser)
 
 model.username = currentUser
