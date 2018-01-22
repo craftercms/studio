9daab19725f2594aa19e23deb2ac160d6a35e7a5
jdrossl <jdross17@gmail.com>
2018-01-22 15:57:42 -0600

    Updated error handling in groovy scripts to send http status codes

--- src/main/webapp/default-site/scripts/rest/api/1/aws/elastictranscoder/transcode.post.groovy
+++ src/main/webapp/default-site/scripts/rest/api/1/aws/elastictranscoder/transcode.post.groovy
@@ -5,8 +5,8 @@ def elasticTranscoderService = applicationContext["studioElasticTranscoderServic
 
 response.setHeader("Content-Type", "text/html")
 
-def sendError = { msg ->
-
+def sendError = { code, msg ->
+    response.status = code
     def writer = response.writer
         writer.println("<script>document.domain = \"${request.serverName}\";</script>")
         writer.println("{\"hasError\":true,\"errors\":[\"${msg}\"]}")
@@ -32,9 +32,9 @@ if (request instanceof MultipartRequest) {
     } catch (e) {
         logger.error("Transcoding of file ${filename} failed", e)
 
-        sendError("Transcoding of file failed: ${e.message}")
+        sendError(500, "Transcoding of file failed: ${e.message}")
     }
 
 } else {
-    sendError("Request is not of type multi-part")
+    sendError(400, "Request is not of type multi-part")
 }
