0022b180972dfb2827cffd4964ab887745171af7
Carlos Ortiz <carlos.ortiz@craftercms.org>
2018-02-27 18:44:12 -0600

    Minor code improvements.

--- src/main/java/org/craftercms/studio/impl/v1/box/BoxProfileReaderImpl.java
+++ src/main/java/org/craftercms/studio/impl/v1/box/BoxProfileReaderImpl.java
@@ -1,12 +1,5 @@
 package org.craftercms.studio.impl.v1.box;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Optional;
-
 import org.apache.commons.configuration2.HierarchicalConfiguration;
 import org.apache.commons.configuration2.XMLConfiguration;
 import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
@@ -19,89 +12,96 @@ import org.craftercms.studio.api.v1.exception.BoxException;
 import org.craftercms.studio.api.v1.service.content.ContentService;
 import org.springframework.beans.factory.annotation.Required;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * {@inheritDoc}
  */
 public class BoxProfileReaderImpl implements BoxProfileReader {
 
-    public static final String KEY_PROFILE = "profile";
-    public static final String KEY_ID = "id";
-    public static final String KEY_CLIENT_ID = "clientId";
-    public static final String KEY_CLIENT_SECRET = "clientSecret";
-    public static final String KEY_ENTERPRISE_ID = "enterpriseId";
-    public static final String KEY_PUBLIC_KEY_ID = "publicKeyId";
-    public static final String KEY_PRIVATE_KEY_PASS = "privateKeyPassword";
-    public static final String KEY_PRIVATE_KEY_PATH = "privateKeyPath";
-    public static final String KEY_PRIVATE_KEY = "privateKey";
-    public static final String KEY_UPLOAD_FOLDER = "uploadFolder";
+	public static final String KEY_PROFILE = "profile";
+	public static final String KEY_ID = "id";
+	public static final String KEY_CLIENT_ID = "clientId";
+	public static final String KEY_CLIENT_SECRET = "clientSecret";
+	public static final String KEY_ENTERPRISE_ID = "enterpriseId";
+	public static final String KEY_PUBLIC_KEY_ID = "publicKeyId";
+	public static final String KEY_PRIVATE_KEY_PASS = "privateKeyPassword";
+	public static final String KEY_PRIVATE_KEY_PATH = "privateKeyPath";
+	public static final String KEY_PRIVATE_KEY = "privateKey";
+	public static final String KEY_UPLOAD_FOLDER = "uploadFolder";
 
-    /**
-     * The full path of the configuration file in the site repository
-     */
-    protected String configPath;
+	/**
+	 * The full path of the configuration file in the site repository
+	 */
+	protected String configPath;
 
-    protected ContentService contentService;
+	protected ContentService contentService;
 
-    @Required
-    public void setConfigPath(final String configPath) {
-        this.configPath = configPath;
-    }
+	@Required
+	public void setConfigPath(final String configPath) {
+		this.configPath = configPath;
+	}
 
-    @Required
-    public void setContentService(final ContentService contentService) {
-        this.contentService = contentService;
-    }
+	@Required
+	public void setContentService(final ContentService contentService) {
+		this.contentService = contentService;
+	}
 
-    protected HierarchicalConfiguration getConfiguration(String site) throws BoxException {
-        try {
-            InputStream input = contentService.getContent(site, configPath);
-            Parameters params = new Parameters();
-            FileBasedConfigurationBuilder<XMLConfiguration> builder = new FileBasedConfigurationBuilder<>(XMLConfiguration.class);
+	protected HierarchicalConfiguration getConfiguration(String site) throws BoxException {
+		try {
+			InputStream input = contentService.getContent(site, configPath);
+			Parameters params = new Parameters();
+			FileBasedConfigurationBuilder<XMLConfiguration> builder = new FileBasedConfigurationBuilder<>(XMLConfiguration.class);
 
-            XMLConfiguration config = builder.configure(params.xml()).getConfiguration();
-            FileHandler fileHandler = new FileHandler(config);
+			XMLConfiguration config = builder.configure(params.xml()).getConfiguration();
+			FileHandler fileHandler = new FileHandler(config);
 
-            fileHandler.setEncoding("UTF-8");
-            fileHandler.load(input);
+			fileHandler.setEncoding("UTF-8");
+			fileHandler.load(input);
 
-            return config;
-        } catch (Exception e) {
-            throw new BoxException("Unable to read box configuration file", e);
-        }
-    }
+			return config;
+		} catch (Exception e) {
+			throw new BoxException("Unable to read box configuration file", e);
+		}
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public BoxProfile getProfile(final String site, final String profileId) throws BoxException {
-        try {
-            HierarchicalConfiguration config = getConfiguration(site);
-            @SuppressWarnings("unchecked")
-            List<HierarchicalConfiguration> profiles = config.configurationsAt(KEY_PROFILE);
-            Optional<HierarchicalConfiguration> profileConfig = profiles.stream().filter(item -> profileId.equals(item.getString(KEY_ID))).findFirst();
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public BoxProfile getProfile(final String site, final String profileId) throws BoxException {
+		try {
+			HierarchicalConfiguration config = getConfiguration(site);
+			@SuppressWarnings("unchecked")
+			List<HierarchicalConfiguration> profiles = config.configurationsAt(KEY_PROFILE);
+			Optional<HierarchicalConfiguration> profileConfig = profiles.stream().filter(item -> profileId.equals(item.getString(KEY_ID))).findFirst();
 
-            HierarchicalConfiguration profile = profileConfig.orElseThrow(() -> new BoxException("Profile not found"));
+			HierarchicalConfiguration profile = profileConfig.orElseThrow(() -> new BoxException("Profile not found"));
 
-            BoxProfile boxProfile = new BoxProfile();
-            boxProfile.setClientId(profile.getString(KEY_CLIENT_ID));
-            boxProfile.setClientSecret(profile.getString(KEY_CLIENT_SECRET));
-            boxProfile.setEnterpriseId(profile.getString(KEY_ENTERPRISE_ID));
-            String boxPrivateKey = profile.getString(KEY_PRIVATE_KEY);
-            if (StringUtils.isNotBlank(boxPrivateKey)) {
+			BoxProfile boxProfile = new BoxProfile();
+			boxProfile.setClientId(profile.getString(KEY_CLIENT_ID));
+			boxProfile.setClientSecret(profile.getString(KEY_CLIENT_SECRET));
+			boxProfile.setEnterpriseId(profile.getString(KEY_ENTERPRISE_ID));
+			String boxPrivateKey = profile.getString(KEY_PRIVATE_KEY);
+			if (StringUtils.isNotBlank(boxPrivateKey)) {
 				boxProfile.setPrivateKey(boxPrivateKey);
-			}else{
+			} else {
 				boxProfile.setPrivateKey(new String(
 						Files.readAllBytes(Paths.get(profile.getString(KEY_PRIVATE_KEY_PATH)))));
 			}
 
-            boxProfile.setPrivateKeyPassword(profile.getString(KEY_PRIVATE_KEY_PASS));
-            boxProfile.setPublicKeyId(profile.getString(KEY_PUBLIC_KEY_ID));
-            boxProfile.setUploadFolder(profile.getString(KEY_UPLOAD_FOLDER));
-            return boxProfile;
+			boxProfile.setPrivateKeyPassword(profile.getString(KEY_PRIVATE_KEY_PASS));
+			boxProfile.setPublicKeyId(profile.getString(KEY_PUBLIC_KEY_ID));
+			boxProfile.setUploadFolder(profile.getString(KEY_UPLOAD_FOLDER));
+			return boxProfile;
 
-        } catch (IOException e) {
-            throw new BoxException("Unable to read private key file", e);
-        }
-    }
+		} catch (IOException e) {
+			throw new BoxException("Unable to read private key file", e);
+		}
+	}
 }
