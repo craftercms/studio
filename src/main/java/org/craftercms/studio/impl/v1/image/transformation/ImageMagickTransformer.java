275aa80e95c0c043bfbeff8d71e98f776c0717a6
Alfonso Vasquez <alfonso.vasquez@craftersoftware.com>
2018-03-09 16:25:45 -0500

    Added missing javadoc of image transformers.

--- src/main/java/org/craftercms/studio/impl/v1/image/transformation/ImageMagickTransformer.java
+++ src/main/java/org/craftercms/studio/impl/v1/image/transformation/ImageMagickTransformer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2007-2018 Crafter Software Corporation. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.craftercms.studio.impl.v1.image.transformation;
 
 import java.io.IOException;
@@ -14,6 +30,12 @@ import org.craftercms.studio.api.v1.image.transformation.ImageTransformer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Transformer that calls ImageMagick from the command line with the options specified in the parameters.
+ *
+ * @author avasquez
+ * @see <a href="https://www.imagemagick.org/script/index.php">ImageMagick</a>
+ */
 public class ImageMagickTransformer implements ImageTransformer {
 
     private static final Logger logger = LoggerFactory.getLogger(ImageMagickTransformer.class);
--- src/main/java/org/craftercms/studio/impl/v1/image/transformation/TinifyTransformer.java
+++ src/main/java/org/craftercms/studio/impl/v1/image/transformation/TinifyTransformer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2007-2018 Crafter Software Corporation. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.craftercms.studio.impl.v1.image.transformation;
 
 import com.tinify.Source;
@@ -12,6 +28,12 @@ import org.craftercms.studio.api.v1.exception.ImageTransformationException;
 import org.craftercms.studio.api.v1.image.transformation.ImageTransformer;
 import org.springframework.beans.factory.annotation.Required;
 
+/**
+ * Transformer that used the Java client of TinyPNG to compress JPEG/PNG images.
+ *
+ * @author avasquez
+ * @see <a href="https://tinypng.com/developers/reference">TinyPNG</a>
+ */
 public class TinifyTransformer implements ImageTransformer {
 
     @Required
