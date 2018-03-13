cded89009be521b4791d0d40eb027e0a0b52367a
Dejan Brkic <dejan.brkic@craftersoftware.com>
2018-03-13 16:55:25 +0100

    work in progress

--- src/main/webapp/default-site/scripts/api/ContentServices.groovy
+++ src/main/webapp/default-site/scripts/api/ContentServices.groovy
@@ -275,4 +275,34 @@ class ContentServices {
         def contentServicesImpl = ServiceFactory.getContentServices(context)
         return contentServicesImpl.renameFolder(site, path, name)
     }
+
+    /**
+     * Add remote
+     * @param context container for passing request, token and other values that may be needed by the implementation
+     * @param siteId site identifier
+     * @param remoteName remote name
+     * @param remoteUrl remote url
+     * @param authenticationType authentication type
+     * @param remoteUsername remote username
+     * @param remotePassword remote password
+     * @param remoteToken remote token
+     * @param remotePrivateKey remote private key
+     * @return
+     */
+    static addRemote(context, siteId, remoteName, remoteUrl, authenticationType, remoteUsername, remotePassword, remoteToken, remotePrivateKey) {
+        def contentServicesImpl = ServicesFactory.getContentServices(context)
+        return contentServicesImpl.addRemote(siteId, remoteName, remoteUrl, authenticationType, remoteUsername, remotePassword, remoteToken, remotePrivateKey)
+    }
+
+    /**
+     * Remove remote
+     * @param context container for passing request, token and other values that may be needed by the implementation
+     * @param siteId site identifier
+     * @param remoteName remote name
+     * @return
+     */
+    static removeRemote(context, siteId, remoteName) {
+        def contentServiceImpl = ServicesFactory.getContentServices(context)
+        return contentServiceImpl.removeRemote(siteId, remoteName)
+    }
 }	
