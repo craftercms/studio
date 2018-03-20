9e0a06c2eb66c3ac26e0c8cb926cc9e152018f53
Dejan Brkic <dejan.brkic@craftersoftware.com>
2018-03-20 17:49:13 +0100

    review fixes

--- src/main/java/org/craftercms/studio/impl/v1/dal/DataSourceInitializerImpl.java
+++ src/main/java/org/craftercms/studio/impl/v1/dal/DataSourceInitializerImpl.java
@@ -45,6 +45,7 @@ public class DataSourceInitializerImpl implements DataSourceInitializer, Disposa
     private final static Logger logger = LoggerFactory.getLogger(DataSourceInitializerImpl.class);
     private final static String CURRENT_DB_VERSION = "3.0.10";
     private final static String DB_VERSION_3_0_10 = "3.0.10";
+    private final static String DB_VERSION_3_0_2_1 = "3.0.2.1";
     private final static String DB_VERSION_3_0_2 = "3.0.2";
     private final static String DB_VERSION_3_0_1 = "3.0.1";
     private final static String DB_VERSION_3_0_0 = "3.0.0";
