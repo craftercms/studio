6a05980769fc94b0baa90a2ad4a15c9280f8dec1
Dejan Brkic <dejan.brkic@craftersoftware.com>
2018-04-30 20:02:52 +0200

    Fixed copyright

--- src/main/webapp/default-site/scripts/rest/api/1/site/get-canned-message.get.groovy
+++ src/main/webapp/default-site/scripts/rest/api/1/site/get-canned-message.get.groovy
@@ -1,6 +1,6 @@
 /*
  * Crafter Studio Web-content authoring solution
- * Copyright (C) 2007-2016 Crafter Software Corporation.
+ * Copyright (C) 2007-2018 Crafter Software Corporation. All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
