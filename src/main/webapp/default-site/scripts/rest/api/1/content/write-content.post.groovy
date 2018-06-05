47a217042223939ce04115c5216bd8ae3b825060
Dejan Brkic <dejan.brkic@craftersoftware.com>
2018-06-05 22:08:58 +0200

    Fixed issue

--- src/main/webapp/default-site/scripts/rest/api/1/content/write-content.post.groovy
+++ src/main/webapp/default-site/scripts/rest/api/1/content/write-content.post.groovy
@@ -86,15 +86,15 @@ if(ServletFileUpload.isMultipartContent(request)) {
         }
     }
 } else {
-    site = params.site_id
-    path = params.path
-    oldPath = params.oldContentPath
-    fileName = (params.fileName) ? params.fileName : params.filename
-    contentType = params.contentType
-    createFolders = params.createFolders
-    edit = params.edit
-    draft = params.draft
-    unlock = params.unlock
+    site = request.getParameter("site_id")
+    path = request.getParameter("path")
+    oldPath = request.getParameter("oldContentPath")
+    fileName = (request.getParameter("fileName")) ? request.getParameter("fileName") : request.getParameter("filename")
+    contentType = request.getParameter("contentType")
+    createFolders = request.getParameter("createFolders")
+    edit = request.getParameter("edit")
+    draft = request.getParameter("draft")
+    unlock = request.getParameter("unlock")
     content = request.getInputStream()
 
     /** Validate Parameters */
@@ -104,7 +104,7 @@ if(ServletFileUpload.isMultipartContent(request)) {
     // site_id
     try {
         if (StringUtils.isEmpty(site)) {
-            site = params.site
+            site = request.getParameter("site")
             if (StringUtils.isEmpty(site)) {
                 invalidParams = true
                 paramsList.add("site_id")
