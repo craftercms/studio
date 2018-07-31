37b309e3996a811581e6336f4875e42452efebd5
Dejan Brkic <dejan.brkic@craftersoftware.com>
2018-07-31 18:17:15 +0200

    Database scripts completed

--- src/main/resources/crafter/studio/database/upgrade-3.0.17.sql
+++ src/main/resources/crafter/studio/database/upgrade-3.0.17.sql
@@ -1,11 +1,3 @@
-ALTER TABLE `remote_repository` ADD UNIQUE `uq_rr_site_remote_name` (`site_id`, `remote_name`) ;
-
-ALTER TABLE `item_metadata` ADD COLUMN `submittedtoenvironment` VARCHAR(255) NULL ;
-
-ALTER TABLE `site` ADD COLUMN `sandbox_branch` VARCHAR(255) NOT NULL DEFAULT 'master' ;
-
-ALTER TABLE `_meta` ADD COLUMN `integrity` BIGINT(10) NULL ;
-
 ALTER TABLE `group`
   DROP FOREIGN KEY `group_site_fk`,
   DROP INDEX `uq_group_name_siteid` ;
