bfd1d7a37ca1f2912a32f93eaef50fb00e9e22cd
Dejan Brkic <dejan.brkic@craftersoftware.com>
2018-08-02 17:35:37 +0200

    work in progress

--- src/main/java/org/craftercms/studio/impl/v1/dal/DataSourceInitializerImpl.java
+++ src/main/java/org/craftercms/studio/impl/v1/dal/DataSourceInitializerImpl.java
@@ -21,6 +21,7 @@ package org.craftercms.studio.impl.v1.dal;
 import ch.vorburger.exec.ManagedProcessException;
 import ch.vorburger.mariadb4j.DB;
 import ch.vorburger.mariadb4j.MariaDB4jService;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -45,13 +46,26 @@ import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.Status;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.springframework.beans.factory.DisposableBean;
 import org.xml.sax.SAXException;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
@@ -324,15 +338,14 @@ public class DataSourceInitializerImpl implements DataSourceInitializer, Disposa
                         }
                     }
 
-                    contentRepository.writeContent(siteId, siteRoleMappingsConfigFullPath, IOUtils.toInputStream(
-                            document.asXML()));
+                    writeToRepo(siteId, siteRoleMappingsConfigFullPath, IOUtils.toInputStream(document.asXML()));
+                    //contentRepository.writeContent(siteId, siteRoleMappingsConfigFullPath, IOUtils.toInputStream(
+                    //        document.asXML()));
 
                 } else {
                     logger.error("Permission mapping not found for " + siteId + ":" + filename);
                 }
             } catch (DocumentException e) {
-                e.printStackTrace();
-            } catch (ServiceException e) {
                 e.printStackTrace();
             } finally {
                 try {
@@ -345,6 +358,96 @@ public class DataSourceInitializerImpl implements DataSourceInitializer, Disposa
                 }
             }
         }
+    }
+
+    private void writeToRepo(String site, String path, InputStream content) {
+        try {
+            Path repositoryPath = Paths.get(studioConfiguration.getProperty(StudioConfiguration.REPO_BASE_PATH),
+                    studioConfiguration.getProperty(StudioConfiguration.SITES_REPOS_PATH), site,
+                    studioConfiguration.getProperty(StudioConfiguration.SANDBOX_PATH));
+            FileRepositoryBuilder builder = new FileRepositoryBuilder();
+            Repository repo = builder
+                    .setGitDir(repositoryPath.toFile())
+                    .readEnvironment()
+                    .findGitDir()
+                    .build();
+
+            // Create basic file
+            File file = new File(repo.getDirectory().getParent(), path);
+
+            String gitPath = getGitPath(path);
+
+            // Create parent folders
+            File folder = file.getParentFile();
+            if (folder != null) {
+                if (!folder.exists()) {
+                    folder.mkdirs();
+                }
+            }
+
+            // Create the file if it doesn't exist already
+            if (!file.exists()) {
+                try {
+                    if (!file.createNewFile()) {
+                        logger.error("error creating file: site: " + site + " path: " + path);
+                    }
+                } catch (IOException e) {
+                    logger.error("error creating file: site: " + site + " path: " + path, e);
+                }
+            }
+
+            // Write the bits
+            try (FileChannel outChannel = new FileOutputStream(file.getPath()).getChannel()) {
+                logger.debug("created the file output channel");
+                ReadableByteChannel inChannel = Channels.newChannel(content);
+                logger.debug("created the file input channel");
+                long amount = 1024 * 1024; // 1MB at a time
+                long count;
+                long offset = 0;
+                while ((count = outChannel.transferFrom(inChannel, offset, amount)) > 0) {
+                    logger.debug("writing the bits: offset = " + offset + " count: " + count);
+                    offset += count;
+                }
+            }
+
+
+            // Add the file to git
+            try (Git git = new Git(repo)) {
+                git.add().addFilepattern(gitPath).call();
+
+                Status status = git.status().addPath(gitPath).call();
+
+                // TODO: SJ: Below needs more thought and refactoring to detect issues with git repo and report them
+                if (status.hasUncommittedChanges() || !status.isClean()) {
+                    RevCommit commit;
+                    commit = git.commit().setOnly(gitPath).setMessage("Upgrade Database").call();
+                    String commitId = commit.getName();
+                }
+
+                git.close();
+            } catch (GitAPIException e) {
+                logger.error("error adding file to git: site: " + site + " path: " + path, e);
+            }
+
+        } catch (IOException e) {
+            logger.error("error writing file: site: " + site + " path: " + path, e);
+        }
+    }
+
+    private String getGitPath(String path) {
+        Path gitPath = Paths.get(path);
+        gitPath = gitPath.normalize();
+        try {
+            gitPath = Paths.get(FILE_SEPARATOR).relativize(gitPath);
+        } catch (IllegalArgumentException e) {
+            logger.debug("Path: " + path + " is already relative path.");
+        }
+        if (StringUtils.isEmpty(gitPath.toString())) {
+            return ".";
+        }
+        String toRet = gitPath.toString();
+        toRet = FilenameUtils.separatorsToUnix(toRet);
+        return toRet;
     }
 
     public boolean isEnabled() {
