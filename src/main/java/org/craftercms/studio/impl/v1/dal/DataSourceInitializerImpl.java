06894b701625ecf5bfaba575df0b90720e9ac865
jdrossl <jdross17@gmail.com>
2018-08-09 15:42:43 -0600

    Fixed current db version

--- src/main/java/org/craftercms/studio/impl/v1/dal/DataSourceInitializerImpl.java
+++ src/main/java/org/craftercms/studio/impl/v1/dal/DataSourceInitializerImpl.java
@@ -60,7 +60,7 @@ import static org.craftercms.studio.api.v1.util.StudioConfiguration.DB_INITIALIZ
 public class DataSourceInitializerImpl implements DataSourceInitializer, DisposableBean {
 
     private final static Logger logger = LoggerFactory.getLogger(DataSourceInitializerImpl.class);
-    private final static String CURRENT_DB_VERSION = "3.0.15.1";
+    private final static String CURRENT_DB_VERSION = "3.0.17";
     private final static String DB_VERSION_3_0_0 = "3.0.0";
     private final static String DB_VERSION_2_5_X = "2.5.x";
 
