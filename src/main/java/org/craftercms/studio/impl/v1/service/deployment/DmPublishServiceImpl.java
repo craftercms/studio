0d61910aad6366b7dc49ffbdabbf2f6c5aa805cb
Dejan Brkic <dejan.brkic@craftersoftware.com>
2018-08-29 19:59:35 +0200

    Appled requested changes

--- src/main/java/org/craftercms/studio/impl/v1/service/deployment/DmPublishServiceImpl.java
+++ src/main/java/org/craftercms/studio/impl/v1/service/deployment/DmPublishServiceImpl.java
@@ -140,7 +140,7 @@ public class DmPublishServiceImpl extends AbstractRegistrableService implements 
     @Override
     @ValidateParams
     public void bulkGoLive(@ValidateStringParam(name = "site") String site, @ValidateStringParam String environment, @ValidateSecurePathParam(name = "path") String path) throws ServiceException {
-        logger.info("Starting Bulk Go Live for path " + path + " site " + site);
+        logger.info("Starting Bulk Publish to '" + environment + "' for path " + path + " site " + site);
 
         String queryPath = path;
         if (queryPath.startsWith(FILE_SEPARATOR + DmConstants.INDEX_FILE)) {
@@ -172,18 +172,18 @@ public class DmPublishServiceImpl extends AbstractRegistrableService implements 
                 }
                 String aprover = securityService.getCurrentUser();
                 String comment = "Bulk Go Live invoked by " + aprover;
-                logger.info("Deploying package of " + pathsToPublish.size() + " items for site " + site + " path " +
-                             childPath);
+                logger.info("Deploying package of " + pathsToPublish.size() + " items to '" + environment +
+                                "' for site" + site + " path " + childPath);
                 try {
                     deploymentService.deploy(site, environment, pathsToPublish, launchDate, aprover, comment, true);
                 } catch (DeploymentException e) {
-                    logger.error("Error while running bulk Go Live operation", e);
+                    logger.error("Error while running Bulk Publish operation", e);
                 } finally {
                     logger.debug("Finished processing deployment package for path " + childPath + " site " + site);
                 }
             }
         }
-        logger.info("Finished Bulk Go Live for path " + path + " site " + site);
+        logger.info("Finished Bulk Publish to '" + environment + "' for path " + path + " site " + site);
     }
 
     public void setDeploymentService(DeploymentService deploymentService) {
