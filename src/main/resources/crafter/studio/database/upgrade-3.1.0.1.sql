30c4d9f5b6662697790ab3b7ffa1ef3740098d52
Dejan Brkic <dejan.brkic@craftersoftware.com>
2018-09-07 20:41:44 +0200

    Adding default groups on upgrade

--- src/main/resources/crafter/studio/database/upgrade-3.1.0.1.sql
+++ src/main/resources/crafter/studio/database/upgrade-3.1.0.1.sql
@@ -1,4 +1,19 @@
 UPDATE `group`
 SET group_name = LCASE(group_name) ;
 
+INSERT IGNORE INTO `group` (record_last_updated, org_id, group_name, group_description)
+VALUES (CURRENT_TIMESTAMP, 1, 'site_admin', 'Site Administrator group') ;
+
+INSERT IGNORE INTO `group` (id, record_last_updated, org_id, group_name, group_description)
+VALUES (CURRENT_TIMESTAMP, 1, 'site_author', 'Site Author group') ;
+
+INSERT IGNORE INTO `group` (id, record_last_updated, org_id, group_name, group_description)
+VALUES (CURRENT_TIMESTAMP, 1, 'site_publisher', 'Site Publisher group') ;
+
+INSERT IGNORE INTO `group` (id, record_last_updated, org_id, group_name, group_description)
+VALUES (CURRENT_TIMESTAMP, 1, 'site_developer', 'Site Developer group') ;
+
+INSERT IGNORE INTO `group` (id, record_last_updated, org_id, group_name, group_description)
+VALUES (CURRENT_TIMESTAMP, 1, 'site_reviewer', 'Site Reviewer group') ;
+
 UPDATE _meta SET version = '3.1.0.2' ;
