23d7cd25a9bbe66d4953de9e65cacdb5c05dd456
Dejan Brkic <dejan.brkic@craftersoftware.com>
2018-09-07 21:57:12 +0200

    fix sql error

--- src/main/resources/crafter/studio/database/createDDL.sql
+++ src/main/resources/crafter/studio/database/createDDL.sql
@@ -23,7 +23,7 @@ CREATE TABLE _meta (
   PRIMARY KEY (`version`)
 ) ;
 
-INSERT INTO _meta (version) VALUES ('3.1.0') ;
+INSERT INTO _meta (version) VALUES ('3.1.0.2') ;
 
 CREATE TABLE IF NOT EXISTS `audit` (
   `id`             BIGINT(20)   NOT NULL AUTO_INCREMENT,
--- src/main/resources/crafter/studio/database/upgrade-3.1.0.1.sql
+++ src/main/resources/crafter/studio/database/upgrade-3.1.0.1.sql
@@ -4,16 +4,16 @@ SET group_name = LCASE(group_name) ;
 INSERT IGNORE INTO `group` (record_last_updated, org_id, group_name, group_description)
 VALUES (CURRENT_TIMESTAMP, 1, 'site_admin', 'Site Administrator group') ;
 
-INSERT IGNORE INTO `group` (id, record_last_updated, org_id, group_name, group_description)
+INSERT IGNORE INTO `group` (record_last_updated, org_id, group_name, group_description)
 VALUES (CURRENT_TIMESTAMP, 1, 'site_author', 'Site Author group') ;
 
-INSERT IGNORE INTO `group` (id, record_last_updated, org_id, group_name, group_description)
+INSERT IGNORE INTO `group` (record_last_updated, org_id, group_name, group_description)
 VALUES (CURRENT_TIMESTAMP, 1, 'site_publisher', 'Site Publisher group') ;
 
-INSERT IGNORE INTO `group` (id, record_last_updated, org_id, group_name, group_description)
+INSERT IGNORE INTO `group` (record_last_updated, org_id, group_name, group_description)
 VALUES (CURRENT_TIMESTAMP, 1, 'site_developer', 'Site Developer group') ;
 
-INSERT IGNORE INTO `group` (id, record_last_updated, org_id, group_name, group_description)
+INSERT IGNORE INTO `group` (record_last_updated, org_id, group_name, group_description)
 VALUES (CURRENT_TIMESTAMP, 1, 'site_reviewer', 'Site Reviewer group') ;
 
 UPDATE _meta SET version = '3.1.0.2' ;
