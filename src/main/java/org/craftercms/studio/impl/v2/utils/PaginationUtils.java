a482f991878f3a7b3b3e9eb99c5aa8d49dc8409e
Alfonso Vasquez <alfonso.vasquez@craftersoftware.com>
2018-09-19 10:49:47 -0400

    Added pagination to /api/2/user/sites.

---
+++ src/main/java/org/craftercms/studio/impl/v2/utils/PaginationUtils.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2007-2018 Crafter Software Corporation. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.craftercms.studio.impl.v2.utils;
+
+import org.apache.commons.beanutils.BeanComparator;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Utility class with methods for pagination.
+ *
+ * @author avasquez
+ */
+public class PaginationUtils {
+
+    private PaginationUtils() {
+    }
+
+    /**
+     * Performs pagination on the {@code list}, returning from the specified {@code offset} to the specified
+     * {@code limit}. The list can also be sorted by the {@code sortBy} (optional).
+     *
+     * @param list      the list to paginate
+     * @param offset    the offset from where to start
+     * @param limit     the max number of elements that the paginated list should include
+     * @param sortBy    the property used for sorting
+     *
+     * @return the paginated list
+     */
+    public static <T> List<T> paginate(List<T> list, int offset, int limit, String sortBy) {
+        Stream<T> stream = list.stream();
+
+        if (StringUtils.isNotEmpty(sortBy)) {
+            stream = stream.sorted(new BeanComparator<>(sortBy));
+        }
+
+        return stream.skip(offset).limit(limit).collect(Collectors.toList());
+    }
+
+}
