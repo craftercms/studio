a7725f0024de29833232a910c87cda2e920b2646
Dejan Brkic <dejan.brkic@craftersoftware.com>
2019-04-05 22:32:43 +0200

    Generic rename upgrade operation

--- src/main/java/org/craftercms/studio/impl/v2/upgrade/operations/BlueprintRenameFolderUpgradeOperation.java
+++ src/main/java/org/craftercms/studio/impl/v2/upgrade/operations/BlueprintRenameFolderUpgradeOperation.java
@@ -33,39 +33,43 @@ import static org.craftercms.studio.api.v1.util.StudioConfiguration.BLUE_PRINTS_
  */
 public class BlueprintRenameFolderUpgradeOperation extends AbstractUpgradeOperation {
 
-    public static final String CONFIG_KEY_BLUEPRINT_FOLDER_NAME = "blueprintFolderName";
-    public static final String CONFIG_KEY_NEW_FOLDER_NAME = "newFolderName";
+    public static final String CONFIG_KEY_SITE = "site";
+    public static final String CONFIG_KEY_OLD_PATH = "oldPath";
+    public static final String CONFIG_KEY_NEW_PATH = "newPath";
 
-    protected String blueprintFolderName;
-    protected String newFolderName;
+    protected String siteId;
+    protected String oldPath;
+    protected String newPath;
 
     /**
      * {@inheritDoc}
      */
     @Override
     public void doInit(final Configuration config) {
-        blueprintFolderName = config.getString(CONFIG_KEY_BLUEPRINT_FOLDER_NAME);
-        newFolderName = config.getString(CONFIG_KEY_NEW_FOLDER_NAME);
+        siteId = config.getString(CONFIG_KEY_SITE);
+        oldPath = config.getString(CONFIG_KEY_OLD_PATH);
+        newPath = config.getString(CONFIG_KEY_NEW_PATH);
     }
 
     @Override
     public void execute(final String site) throws UpgradeException {
         try {
-            Path globalConfigPath = getRepositoryPath(StringUtils.EMPTY);
-            Path blueprintPath = Paths.get(globalConfigPath.toAbsolutePath().toString(),
-                    studioConfiguration.getProperty(BLUE_PRINTS_PATH), blueprintFolderName);
-            File blueprintFolder = blueprintPath.toFile();
-            if (blueprintFolder.exists()) {
-                Path newBlueprintPath = Paths.get(globalConfigPath.toAbsolutePath().toString(),
-                        studioConfiguration.getProperty(BLUE_PRINTS_PATH), newFolderName);
-                File newBlueprintFolder = newBlueprintPath.toFile();
-                FileUtils.deleteDirectory(newBlueprintFolder);
-                blueprintFolder.renameTo(newBlueprintFolder);
+
+            Path oldP = Paths.get(getRepositoryPath(siteId).toAbsolutePath().toString(), oldPath);
+            Path newP = Paths.get(getRepositoryPath(siteId).toAbsolutePath().toString(), newPath);
+
+            File oldF = oldP.toFile();
+            File newF = newP.toFile();
+            if (oldF.isDirectory()) {
+                FileUtils.moveDirectory(oldF, newF);
+            } else if (oldF.isFile()) {
+                FileUtils.moveFile(oldF, newF);
             }
 
-            commitAllChanges(StringUtils.EMPTY);
+            commitAllChanges(siteId);
         } catch (Exception e) {
-            throw new UpgradeException("Error upgrading blueprints in the global repo", e);
+            throw new UpgradeException("Error moving path " + oldPath + " to path " + newPath + " for repo " +
+                    (StringUtils.isEmpty(siteId) ? "global" : siteId), e);
         }
     }
 
