41ab7b2448ee3e691d6ae2f4a165a41723a444f1
Dejan Brkic <dejan.brkic@craftersoftware.com>
2019-04-05 18:18:34 +0200

    Requested changes

--- src/main/java/org/craftercms/studio/impl/v2/upgrade/operations/BlueprintRenameFolderUpgradeOperation.java
+++ src/main/java/org/craftercms/studio/impl/v2/upgrade/operations/BlueprintRenameFolderUpgradeOperation.java
@@ -19,35 +19,25 @@ package org.craftercms.studio.impl.v2.upgrade.operations;
 
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.io.FileUtils;
-import org.craftercms.studio.api.v1.constant.GitRepositories;
+import org.apache.commons.lang3.StringUtils;
 import org.craftercms.studio.api.v1.log.Logger;
 import org.craftercms.studio.api.v1.log.LoggerFactory;
 import org.craftercms.studio.api.v1.service.configuration.ServicesConfig;
-import org.craftercms.studio.api.v1.service.security.SecurityService;
 import org.craftercms.studio.api.v2.exception.UpgradeException;
-import org.craftercms.studio.api.v2.service.security.internal.UserServiceInternal;
-import org.craftercms.studio.impl.v1.repository.git.GitContentRepositoryHelper;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.Status;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.lib.Repository;
 import org.springframework.beans.factory.annotation.Required;
-import org.springframework.web.context.ServletContextAware;
 
 import javax.servlet.ServletContext;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import static org.craftercms.studio.api.v1.util.StudioConfiguration.BLUE_PRINTS_PATH;
-import static org.craftercms.studio.api.v1.util.StudioConfiguration.REPO_BLUEPRINTS_UPDATED_COMMIT_MESSAGE;
-import static org.craftercms.studio.impl.v1.repository.git.GitContentRepositoryConstants.GIT_COMMIT_ALL_ITEMS;
 
 /**
  * Implementation of {@link org.craftercms.studio.api.v2.upgrade.UpgradeOperation} that renames the blueprint in the
  * global repository.
  * @author Dejan Brkic
  */
-public class BlueprintRenameFolderUpgradeOperation extends AbstractUpgradeOperation implements ServletContextAware {
+public class BlueprintRenameFolderUpgradeOperation extends AbstractUpgradeOperation {
 
     private static final Logger logger = LoggerFactory.getLogger(BlueprintRenameFolderUpgradeOperation.class);
 
@@ -57,8 +47,6 @@ public class BlueprintRenameFolderUpgradeOperation extends AbstractUpgradeOperat
 
     protected ServletContext servletContext;
     protected ServicesConfig servicesConfig;
-    protected SecurityService securityService;
-    protected UserServiceInternal userServiceInternal;
 
     protected String blueprintFolderName;
     protected String newFolderName;
@@ -73,19 +61,6 @@ public class BlueprintRenameFolderUpgradeOperation extends AbstractUpgradeOperat
         this.servicesConfig = servicesConfig;
     }
 
-    public SecurityService getSecurityService() {
-        return securityService;
-    }
-
-    public void setSecurityService(SecurityService securityService) {
-        this.securityService = securityService;
-    }
-
-
-    public void setUserServiceInternal(UserServiceInternal userServiceInternal) {
-        this.userServiceInternal = userServiceInternal;
-    }
-
     /**
      * {@inheritDoc}
      */
@@ -98,9 +73,7 @@ public class BlueprintRenameFolderUpgradeOperation extends AbstractUpgradeOperat
     @Override
     public void execute(final String site) throws UpgradeException {
         try {
-            GitContentRepositoryHelper helper =
-                new GitContentRepositoryHelper(studioConfiguration, servicesConfig, userServiceInternal, securityService);
-            Path globalConfigPath = helper.buildRepoPath(GitRepositories.GLOBAL);
+            Path globalConfigPath = getRepositoryPath(StringUtils.EMPTY);
             Path blueprintPath = Paths.get(globalConfigPath.toAbsolutePath().toString(),
                     studioConfiguration.getProperty(BLUE_PRINTS_PATH), blueprintFolderName);
             File blueprintFolder = blueprintPath.toFile();
@@ -112,22 +85,7 @@ public class BlueprintRenameFolderUpgradeOperation extends AbstractUpgradeOperat
                 blueprintFolder.renameTo(newBlueprintFolder);
             }
 
-            Repository globalRepo = helper.getRepository(site, GitRepositories.GLOBAL);
-            try (Git git = new Git(globalRepo)) {
-
-                Status status = git.status().call();
-
-                if (status.hasUncommittedChanges() || !status.isClean()) {
-                    // Commit everything
-                    // TODO: Consider what to do with the commitId in the future
-                    git.add().addFilepattern(GIT_COMMIT_ALL_ITEMS).call();
-                    git.commit()
-                            .setAll(true)
-                            .setMessage(studioConfiguration.getProperty(REPO_BLUEPRINTS_UPDATED_COMMIT_MESSAGE)).call();
-                }
-            } catch (GitAPIException err) {
-                logger.error("error creating initial commit for global configuration", err);
-            }
+            commitAllChanges(StringUtils.EMPTY);
         } catch (Exception e) {
             throw new UpgradeException("Error upgrading blueprints in the global repo", e);
         }
