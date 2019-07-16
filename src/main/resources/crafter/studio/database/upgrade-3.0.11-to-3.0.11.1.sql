2a50e105186303368d8e4bb6a801efdbc15919d6
Dejan Brkic <dejan.brkic@craftersoftware.com>
2019-07-16 17:52:06 +0200

    work in progress

--- src/main/resources/crafter/studio/database/upgrade-3.0.11-to-3.0.11.1.sql
+++ src/main/resources/crafter/studio/database/upgrade-3.0.11-to-3.0.11.1.sql
@@ -1,4 +1,25 @@
-ALTER TABLE `remote_repository` DROP COLUMN IF EXISTS `remote_branch` ;
+DROP PROCEDURE IF EXISTS dropColumnIfExists ;
+
+CREATE PROCEDURE dropColumnIfExists(
+    IN schemaName tinytext,
+    IN tableName tinytext,
+    IN columnName tinytext)
+  BEGIN
+    IF EXISTS (
+            SELECT * FROM information_schema.COLUMNS
+            WHERE column_name = columnName
+              AND table_name = tableName
+              AND table_schema = schemaName
+        )
+    THEN
+        SET @dropColumn=CONCAT('ALTER TABLE ', schemaName, '.', tableName,
+                              ' DROP COLUMN ', columnName);
+        PREPARE statement FROM @dropColumn;
+        EXECUTE statement;
+    END IF;
+  END ;
+
+call dropColumnIfExists('crafter', 'remote_repository', 'remote_branch') ;
 
 UPDATE `audit` SET `source` = 'API' WHERE `source` = 'UI' ;
 
