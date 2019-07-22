1be13ef6fe471ada61db174664a3d03806c5c411
Veronica Estrada <mariave011@gmail.com>
2019-07-22 05:58:58 -0600

    https://github.com/craftercms/craftercms/issues/3271 - [studio] Default site blueprints should enforce postfixes by default and have all content type fields with postfixes #3271

--- src/main/webapp/repo-bootstrap/global/blueprints/1000_website_editorial/scripts/pages/category-landing.groovy
+++ src/main/webapp/repo-bootstrap/global/blueprints/1000_website_editorial/scripts/pages/category-landing.groovy
@@ -19,7 +19,7 @@ import org.craftercms.sites.editorial.SearchHelper
 import org.craftercms.sites.editorial.ProfileUtils
 
 def segment = ProfileUtils.getSegment(profile, siteItemService)
-def category = contentModel.category.text
+def category = contentModel.category_s.text
 def maxArticles = contentModel.max_articles_i.text as Integer
 def searchHelper = new SearchHelper(elasticsearch, urlTransformationService)
 def articles = searchHelper.searchArticles(false, category, segment, 0, maxArticles)
