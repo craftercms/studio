4e25cb321ad4945f6968286f804b62105a89d67d
Dejan Brkic <dejan.brkic@craftersoftware.com>
2019-07-26 17:25:37 +0200

    Added audit log on update configuration

--- src/main/java/org/craftercms/studio/impl/v2/service/configuration/ConfigurationServiceImpl.java
+++ src/main/java/org/craftercms/studio/impl/v2/service/configuration/ConfigurationServiceImpl.java
@@ -22,12 +22,19 @@ import org.craftercms.commons.lang.UrlUtils;
 import org.craftercms.commons.security.permissions.DefaultPermission;
 import org.craftercms.commons.security.permissions.annotations.HasPermission;
 import org.craftercms.commons.security.permissions.annotations.ProtectedResourceId;
+import org.craftercms.studio.api.v1.dal.SiteFeed;
 import org.craftercms.studio.api.v1.exception.ServiceLayerException;
+import org.craftercms.studio.api.v1.exception.SiteNotFoundException;
 import org.craftercms.studio.api.v1.log.Logger;
 import org.craftercms.studio.api.v1.log.LoggerFactory;
 import org.craftercms.studio.api.v1.service.content.ContentService;
+import org.craftercms.studio.api.v1.service.content.ObjectMetadataManager;
+import org.craftercms.studio.api.v1.service.security.SecurityService;
+import org.craftercms.studio.api.v1.service.site.SiteService;
 import org.craftercms.studio.api.v1.util.StudioConfiguration;
+import org.craftercms.studio.api.v2.dal.AuditLog;
 import org.craftercms.studio.api.v2.exception.ConfigurationException;
+import org.craftercms.studio.api.v2.service.audit.internal.AuditServiceInternal;
 import org.craftercms.studio.api.v2.service.config.ConfigurationService;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
@@ -40,11 +47,14 @@ import org.xml.sax.SAXException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Paths;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.craftercms.studio.api.v1.constant.StudioConstants.CONTENT_TYPE_CONFIGURATION;
 import static org.craftercms.studio.api.v1.constant.StudioConstants.FILE_SEPARATOR;
 import static org.craftercms.studio.api.v1.constant.StudioConstants.PATTERN_ENVIRONMENT;
 import static org.craftercms.studio.api.v1.constant.StudioConstants.PATTERN_MODULE;
@@ -53,12 +63,17 @@ import static org.craftercms.studio.api.v1.constant.StudioXmlConstants.DOCUMENT_
 import static org.craftercms.studio.api.v1.constant.StudioXmlConstants.DOCUMENT_ELM_GROUPS_NODE;
 import static org.craftercms.studio.api.v1.constant.StudioXmlConstants.DOCUMENT_ELM_PERMISSION_ROLE;
 import static org.craftercms.studio.api.v1.constant.StudioXmlConstants.DOCUMENT_ROLE_MAPPINGS;
+import static org.craftercms.studio.api.v1.dal.ItemMetadata.PROP_LOCK_OWNER;
+import static org.craftercms.studio.api.v1.dal.ItemMetadata.PROP_MODIFIED;
+import static org.craftercms.studio.api.v1.dal.ItemMetadata.PROP_MODIFIER;
 import static org.craftercms.studio.api.v1.util.StudioConfiguration.CONFIGURATION_ENVIRONMENT_ACTIVE;
 import static org.craftercms.studio.api.v1.util.StudioConfiguration.CONFIGURATION_SITE_CONFIG_BASE_PATH;
 import static org.craftercms.studio.api.v1.util.StudioConfiguration.CONFIGURATION_SITE_CONFIG_BASE_PATH_PATTERN;
 import static org.craftercms.studio.api.v1.util.StudioConfiguration.CONFIGURATION_SITE_MUTLI_ENVIRONMENT_CONFIG_BASE_PATH;
 import static org.craftercms.studio.api.v1.util.StudioConfiguration.CONFIGURATION_SITE_MUTLI_ENVIRONMENT_CONFIG_BASE_PATH_PATTERN;
 import static org.craftercms.studio.api.v1.util.StudioConfiguration.CONFIGURATION_SITE_ROLE_MAPPINGS_FILE_NAME;
+import static org.craftercms.studio.api.v2.dal.AuditLogConstants.OPERATION_UPDATE;
+import static org.craftercms.studio.api.v2.dal.AuditLogConstants.TARGET_TYPE_CONTENT_ITEM;
 import static org.craftercms.studio.permissions.PermissionResolverImpl.SITE_ID_RESOURCE_ID;
 
 
@@ -68,6 +83,10 @@ public class ConfigurationServiceImpl implements ConfigurationService {
 
     private ContentService contentService;
     private StudioConfiguration studioConfiguration;
+    private AuditServiceInternal auditServiceInternal;
+    private SiteService siteService;
+    private SecurityService securityService;
+    private ObjectMetadataManager objectMetadataManager;
 
     @Override
     @SuppressWarnings("unchecked")
@@ -115,7 +134,8 @@ public class ConfigurationServiceImpl implements ConfigurationService {
             siteConfigPath = studioConfiguration.getProperty(CONFIGURATION_SITE_MUTLI_ENVIRONMENT_CONFIG_BASE_PATH)
                     .replaceAll(PATTERN_ENVIRONMENT, studioConfiguration.getProperty(CONFIGURATION_ENVIRONMENT_ACTIVE));
             if (!contentService.contentExists(siteId,siteConfigPath + FILE_SEPARATOR + getSiteRoleMappingsConfigFileName())) {
-                siteConfigPath = studioConfiguration.getProperty(CONFIGURATION_SITE_CONFIG_BASE_PATH).replaceFirst(PATTERN_SITE, siteId);
+                siteConfigPath = studioConfiguration.getProperty(CONFIGURATION_SITE_CONFIG_BASE_PATH)
+                        .replaceFirst(PATTERN_SITE, siteId);
             }
         } else {
             siteConfigPath = studioConfiguration.getProperty(CONFIGURATION_SITE_CONFIG_BASE_PATH)
@@ -190,6 +210,9 @@ public class ConfigurationServiceImpl implements ConfigurationService {
                 .replaceAll(PATTERN_MODULE, module);
         String configPath = Paths.get(configBasePath, path).toString();
         contentService.writeContent(siteId, configPath, content);
+        String currentUser = securityService.getCurrentUser();
+        updateMetadata(siteId, configPath, currentUser);
+        generateAuditLog(siteId, configPath, currentUser);
     }
 
     private void writeEnvironmentConfiguration(String siteId, String module, String path, String environment,
@@ -202,12 +225,39 @@ public class ConfigurationServiceImpl implements ConfigurationService {
             if (contentService.contentExists(siteId, configBasePath)) {
                 String configPath = Paths.get(configBasePath, path).toString();
                 contentService.writeContent(siteId, configPath, content);
+                String currentUser = securityService.getCurrentUser();
+                updateMetadata(siteId, configPath, currentUser);
+                generateAuditLog(siteId, configPath, currentUser);
             } else {
                 writeDefaultConfiguration(siteId, module, path, content);
             }
         } else {
             writeDefaultConfiguration(siteId, module, path, content);
         }
+    }
+
+    private void updateMetadata(String siteId, String path, String user) {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(PROP_MODIFIER, user);
+        properties.put(PROP_MODIFIED, ZonedDateTime.now(ZoneOffset.UTC));
+        properties.put(PROP_LOCK_OWNER, StringUtils.EMPTY);
+        if (!objectMetadataManager.metadataExist(siteId, path)) {
+            objectMetadataManager.insertNewObjectMetadata(siteId, path);
+        }
+        objectMetadataManager.setObjectMetadata(siteId, path, properties);
+    }
+
+    private void generateAuditLog(String siteId, String path, String user) throws SiteNotFoundException {
+        SiteFeed siteFeed = siteService.getSite(siteId);
+        AuditLog auditLog = auditServiceInternal.createAuditLogEntry();
+        auditLog.setOperation(OPERATION_UPDATE);
+        auditLog.setSiteId(siteFeed.getId());
+        auditLog.setActorId(user);
+        auditLog.setPrimaryTargetId(siteId + ":" + path);
+        auditLog.setPrimaryTargetType(TARGET_TYPE_CONTENT_ITEM);
+        auditLog.setPrimaryTargetValue(path);
+        auditLog.setPrimaryTargetSubtype(CONTENT_TYPE_CONFIGURATION);
+        auditServiceInternal.insertAuditLog(auditLog);
     }
 
     @Required
@@ -220,4 +270,35 @@ public class ConfigurationServiceImpl implements ConfigurationService {
         this.studioConfiguration = studioConfiguration;
     }
 
+    public AuditServiceInternal getAuditServiceInternal() {
+        return auditServiceInternal;
+    }
+
+    public void setAuditServiceInternal(AuditServiceInternal auditServiceInternal) {
+        this.auditServiceInternal = auditServiceInternal;
+    }
+
+    public SiteService getSiteService() {
+        return siteService;
+    }
+
+    public void setSiteService(SiteService siteService) {
+        this.siteService = siteService;
+    }
+
+    public SecurityService getSecurityService() {
+        return securityService;
+    }
+
+    public void setSecurityService(SecurityService securityService) {
+        this.securityService = securityService;
+    }
+
+    public ObjectMetadataManager getObjectMetadataManager() {
+        return objectMetadataManager;
+    }
+
+    public void setObjectMetadataManager(ObjectMetadataManager objectMetadataManager) {
+        this.objectMetadataManager = objectMetadataManager;
+    }
 }
