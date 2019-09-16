ef7f7de0f77a3fe4b608c8add233e716a8c5b9d1
Dejan Brkic <dejan.brkic@craftersoftware.com>
2019-09-16 18:29:35 +0200

    fixed issue with scheduled date not being reseted

--- src/main/java/org/craftercms/studio/impl/v1/service/deployment/PublishingManagerImpl.java
+++ src/main/java/org/craftercms/studio/impl/v1/service/deployment/PublishingManagerImpl.java
@@ -222,13 +222,13 @@ public class PublishingManagerImpl implements PublishingManager {
                 } else {
                     objectStateService.transition(site, contentItem, TransitionEvent.SAVE);
                 }
-                Map<String, Object> props = new HashMap<String, Object>();
-                props.put(ItemMetadata.PROP_SUBMITTED_BY, StringUtils.EMPTY);
-                props.put(ItemMetadata.PROP_SEND_EMAIL, 0);
-                props.put(ItemMetadata.PROP_SUBMITTED_FOR_DELETION, 0);
-                props.put(ItemMetadata.PROP_SUBMISSION_COMMENT, StringUtils.EMPTY);
-                props.put(ItemMetadata.PROP_SUBMITTED_TO_ENVIRONMENT, StringUtils.EMPTY);
-                objectMetadataManager.setObjectMetadata(site, path, props);
+                itemMetadata.setSubmittedBy(StringUtils.EMPTY);
+                itemMetadata.setSendEmail(0);
+                itemMetadata.setSubmittedForDeletion(0);
+                itemMetadata.setSubmissionComment(StringUtils.EMPTY);
+                itemMetadata.setSubmittedToEnvironment(StringUtils.EMPTY);
+                itemMetadata.setLaunchDate(null);
+                objectMetadataManager.updateObjectMetadata(itemMetadata);
             }
         }
         return deploymentItem;
