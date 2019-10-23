4ee2079da4eb240bbeb72dc64fdf229e30f9736b
jdrossl <jdross17@gmail.com>
2019-10-23 12:07:28 -0600

    Update API according to feedback

--- src/main/java/org/craftercms/studio/controller/rest/v2/MarketplaceController.java
+++ src/main/java/org/craftercms/studio/controller/rest/v2/MarketplaceController.java
@@ -20,25 +20,13 @@ package org.craftercms.studio.controller.rest.v2;
 import java.util.List;
 import java.util.Map;
 
-import javax.validation.Valid;
-
-import org.craftercms.studio.api.v1.exception.ServiceLayerException;
-import org.craftercms.studio.api.v1.exception.repository.InvalidRemoteRepositoryCredentialsException;
-import org.craftercms.studio.api.v1.exception.repository.InvalidRemoteRepositoryException;
-import org.craftercms.studio.api.v1.exception.repository.InvalidRemoteUrlException;
-import org.craftercms.studio.api.v1.exception.repository.RemoteRepositoryNotBareException;
-import org.craftercms.studio.api.v1.exception.repository.RemoteRepositoryNotFoundException;
 import org.craftercms.studio.api.v2.exception.marketplace.MarketplaceException;
 import org.craftercms.studio.api.v2.service.marketplace.Constants;
 import org.craftercms.studio.api.v2.service.marketplace.MarketplaceService;
 import org.craftercms.studio.model.rest.ApiResponse;
 import org.craftercms.studio.model.rest.PaginatedResultList;
 import org.craftercms.studio.model.rest.ResponseBody;
-import org.craftercms.studio.model.rest.Result;
-import org.craftercms.studio.model.rest.marketplace.CreateSiteRequest;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -78,22 +66,6 @@ public class MarketplaceController {
         result.setTotal((int) page.get(Constants.RESULT_TOTAL));
         result.setOffset((int) offset);
         result.setLimit((int) limit);
-        response.setResult(result);
-
-        return response;
-    }
-
-    @PostMapping("/create_site")
-    public ResponseBody createSite(@Valid @RequestBody CreateSiteRequest request) throws RemoteRepositoryNotFoundException,
-        InvalidRemoteRepositoryException, ServiceLayerException, InvalidRemoteRepositoryCredentialsException,
-        InvalidRemoteUrlException, RemoteRepositoryNotBareException {
-
-        marketplaceService.createSite(request);
-
-        Result result = new Result();
-        result.setResponse(ApiResponse.CREATED);
-
-        ResponseBody response = new ResponseBody();
         response.setResult(result);
 
         return response;
