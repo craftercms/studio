fc3263b8a7407a622eb432e0ec8e78843ced19f0
jdrossl <jdross17@gmail.com>
2019-10-29 11:17:47 -0600

    Upgrade Manager: - Update sample global override config

--- src/main/resources/crafter/studio/database/createDDL.sql
+++ src/main/resources/crafter/studio/database/createDDL.sql
@@ -105,7 +105,7 @@ CREATE TABLE _meta (
   PRIMARY KEY (`version`)
 ) ;
 
-INSERT INTO _meta (version, studio_id) VALUES ('3.1.4.10', UUID()) ;
+INSERT INTO _meta (version, studio_id) VALUES ('3.1.4.12', UUID()) ;
 
 CREATE TABLE IF NOT EXISTS `audit` (
   `id`                        BIGINT(20)    NOT NULL AUTO_INCREMENT,
