af4804a457e8c16248a8834b9f1a0d65e8434c17
Dejan Brkic <dejan.brkic@craftersoftware.com>
2019-11-11 21:03:49 +0100

    Removed password verification switch

--- src/main/resources/crafter/studio/database/createDDL.sql
+++ src/main/resources/crafter/studio/database/createDDL.sql
@@ -105,7 +105,7 @@ CREATE TABLE _meta (
   PRIMARY KEY (`version`)
 ) ;
 
-INSERT INTO _meta (version, studio_id) VALUES ('3.1.4.15', UUID()) ;
+INSERT INTO _meta (version, studio_id) VALUES ('3.1.4.16', UUID()) ;
 
 CREATE TABLE IF NOT EXISTS `audit` (
   `id`                        BIGINT(20)    NOT NULL AUTO_INCREMENT,
