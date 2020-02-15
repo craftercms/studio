ed1992032fc5daa9dec6e06a566f5cdfc15597d7
jdrossl <jdross17@gmail.com>
2020-02-14 18:04:53 -0600

    Add static-assets to public urls

--- src/main/resources/crafter/studio/database/createDDL.sql
+++ src/main/resources/crafter/studio/database/createDDL.sql
@@ -105,7 +105,7 @@ CREATE TABLE _meta (
   PRIMARY KEY (`version`)
 ) ;
 
-INSERT INTO _meta (version, studio_id) VALUES ('3.1.5.6', UUID()) ;
+INSERT INTO _meta (version, studio_id) VALUES ('3.1.5.7', UUID()) ;
 
 CREATE TABLE IF NOT EXISTS `audit` (
   `id`                        BIGINT(20)    NOT NULL AUTO_INCREMENT,
