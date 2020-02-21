5bd4bbe3b3b25237054d85d6ca9590d2867da598
Dejan Brkic <dejan.brkic@craftersoftware.com>
2020-02-21 16:26:22 +0100

    Fixed compile warnings

--- src/main/java/org/craftercms/studio/api/v1/service/dependency/DependencyService.java
+++ src/main/java/org/craftercms/studio/api/v1/service/dependency/DependencyService.java
@@ -1,10 +1,9 @@
 /*
- * Copyright (C) 2007-2019 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2020 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -73,7 +72,7 @@ public interface DependencyService {
 	 * @throws SiteNotFoundException Site doesn't exist
 	 * @throws ContentNotFoundException Path doesn't exist
 	 * @throws ServiceLayerException Internal error, see exception details
-	 * @return
+	 * @return list of mandatory dependencies paths for publishing
 	 */
 	List<String> getPublishingDependencies(String site, String path)
             throws SiteNotFoundException, ContentNotFoundException, ServiceLayerException;
@@ -89,7 +88,7 @@ public interface DependencyService {
 	 * @throws SiteNotFoundException Site doesn't exist
 	 * @throws ContentNotFoundException One or more paths doesn't exist
 	 * @throws ServiceLayerException Internal error, see exception details
-	 * @return
+	 * @return list of mandatory dependencies paths for publishing
 	 */
 	List<String> getPublishingDependencies(String site, List<String> paths)
             throws SiteNotFoundException, ContentNotFoundException, ServiceLayerException;
