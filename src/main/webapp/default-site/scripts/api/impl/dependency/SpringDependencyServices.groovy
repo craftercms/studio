21899d07601e23e7bce05a7d3d0e9846c747fe64
Dejan Brkic <dejan.brkic@craftersoftware.com>
2020-02-26 15:49:49 +0100

    Fixed issue with missing audit log. Fixed secundary issue with duplicate items in reject dialog

--- src/main/webapp/default-site/scripts/api/impl/dependency/SpringDependencyServices.groovy
+++ src/main/webapp/default-site/scripts/api/impl/dependency/SpringDependencyServices.groovy
@@ -1,10 +1,9 @@
 /*
- * Copyright (C) 2007-2019 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2020 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -57,8 +56,7 @@ class SpringDependencyServices {
             def dependencyPaths = springBackendService.getDeleteDependencies(site, paths)
             toProcess.addAll(dependencyPaths)
         } else {
-            def dependencyPaths = springBackendService.getPublishingDependencies(site, paths)
-            toProcess.addAll(dependencyPaths)
+            toProcess = springBackendService.getPublishingDependencies(site, paths)
         }
 
         toProcess.each {
