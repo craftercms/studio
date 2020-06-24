82214f6e27ee0aa71d63d12392bbe1c5ec81afc3
Dejan Brkic <dejan.brkic@craftersoftware.com>
2020-06-24 19:45:36 +0200

    Fixed permissions issue for encryption tool

--- src/main/java/org/craftercms/studio/permissions/StudioPermissions.java
+++ src/main/java/org/craftercms/studio/permissions/StudioPermissions.java
@@ -5,4 +5,7 @@ public interface StudioPermissions {
     /** Publish Service */
     String ACTION_GET_PUBLISHING_QUEUE = "get_publishing_queue";
     String ACTION_CANCEL_PUBLISH = "cancel_publish";
+
+    /** Encryption service */
+    String ACTION_ENCRYPTION_TOOL = "encryption_tool";
 }
