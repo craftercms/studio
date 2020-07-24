e0cbc6771d1f7d93b38009bb0fa0c1b398d73ed2
Dejan Brkic <dejan.brkic@craftersoftware.com>
2020-07-24 21:37:47 +0200

    Remote repositories that are cluster nodes, can not be removed anymore

--- src/main/java/org/craftercms/studio/api/v2/dal/RemoteRepositoryInfo.java
+++ src/main/java/org/craftercms/studio/api/v2/dal/RemoteRepositoryInfo.java
@@ -16,8 +16,12 @@
 
 package org.craftercms.studio.api.v2.dal;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.Serializable;
 import java.util.List;
+
+import static org.craftercms.studio.impl.v1.repository.git.GitContentRepositoryConstants.CLUSTER_NODE_REMOTE_NAME_PREFIX;
 
 public class RemoteRepositoryInfo implements Serializable {
 
@@ -30,6 +34,7 @@ public class RemoteRepositoryInfo implements Serializable {
     private List<String> branches;
     private boolean reachable = true;
     private String unreachableReason;
+    private boolean removable = true;
 
     public String getName() {
         return name;
@@ -37,6 +42,7 @@ public class RemoteRepositoryInfo implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+        this.removable = !StringUtils.startsWith(name, CLUSTER_NODE_REMOTE_NAME_PREFIX);
     }
 
     public String getUrl() {
@@ -85,5 +91,13 @@ public class RemoteRepositoryInfo implements Serializable {
 
     public void setUnreachableReason(String unreachableReason) {
         this.unreachableReason = unreachableReason;
+    }
+
+    public boolean isRemovable() {
+        return removable;
+    }
+
+    public void setRemovable(boolean removable) {
+        // not used
     }
 }
--- src/main/java/org/craftercms/studio/api/v2/service/repository/internal/RepositoryManagementServiceInternal.java
+++ src/main/java/org/craftercms/studio/api/v2/service/repository/internal/RepositoryManagementServiceInternal.java
@@ -19,6 +19,7 @@ package org.craftercms.studio.api.v2.service.repository.internal;
 import org.craftercms.commons.crypto.CryptoException;
 import org.craftercms.studio.api.v1.exception.ServiceLayerException;
 import org.craftercms.studio.api.v1.exception.repository.InvalidRemoteUrlException;
+import org.craftercms.studio.api.v1.exception.repository.RemoteNotRemovableException;
 import org.craftercms.studio.api.v2.dal.DiffConflictedFile;
 import org.craftercms.studio.api.v2.dal.RemoteRepository;
 import org.craftercms.studio.api.v2.dal.RemoteRepositoryInfo;
@@ -40,7 +41,7 @@ public interface RepositoryManagementServiceInternal {
     boolean pushToRemote(String siteId, String remoteName, String remoteBranch, boolean force)
             throws CryptoException, ServiceLayerException, InvalidRemoteUrlException;
 
-    boolean removeRemote(String siteId, String remoteName) throws CryptoException;
+    boolean removeRemote(String siteId, String remoteName) throws CryptoException, RemoteNotRemovableException;
 
     RepositoryStatus getRepositoryStatus(String siteId) throws CryptoException, ServiceLayerException;
 
