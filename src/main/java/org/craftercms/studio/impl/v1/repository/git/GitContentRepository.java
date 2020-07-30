be3ee3e92d5bd06b19518007ef718d5ee1bbb218
Dejan Brkic <dejan.brkic@craftersoftware.com>
2020-07-30 21:52:47 +0200

    Fixed error handling to cover wrong protocol

--- src/main/java/org/craftercms/studio/impl/v1/repository/git/GitContentRepository.java
+++ src/main/java/org/craftercms/studio/impl/v1/repository/git/GitContentRepository.java
@@ -1326,6 +1326,11 @@ public class GitContentRepository implements ContentRepository, ServletContextAw
                         throw new RemoteRepositoryNotFoundException("Remote repository not found: " + remoteName + " (" +
                                 remoteUrl + ")");
                     }
+                } catch (ClassCastException e) {
+                    logger.error("Wrong protocol used to access repository: " + remoteName +
+                            " (" + remoteUrl + ")", e);
+                    throw new InvalidRemoteRepositoryCredentialsException("Wrong protocol used to access repository: " +
+                            remoteName + " (" + remoteUrl + ")", e);
                 } catch (ServiceLayerException | IOException e) {
                     logger.error("Failed to push newly created site " + siteId + " to remote repository " +
                             remoteUrl, e);
@@ -1394,9 +1399,9 @@ public class GitContentRepository implements ContentRepository, ServletContextAw
                     }
                 }
 
-            } catch (URISyntaxException e) {
+            } catch (URISyntaxException | ClassCastException e) {
                 logger.error("Remote URL is invalid " + remoteUrl, e);
-                throw new InvalidRemoteUrlException();
+                throw new InvalidRemoteUrlException("Remote URL is invalid " + remoteUrl, e);
             } catch (GitAPIException | IOException e) {
                 logger.error("Error while adding remote " + remoteName + " (url: " + remoteUrl + ") for site " +
                         siteId, e);
