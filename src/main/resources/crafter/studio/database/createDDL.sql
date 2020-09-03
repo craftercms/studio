72c54894fb741bd9aed823f80f81784ea899ff96
Dejan Brkic <dejan.brkic@craftersoftware.com>
2020-09-03 17:10:54 +0200

    work in progress

--- src/main/resources/crafter/studio/database/createDDL.sql
+++ src/main/resources/crafter/studio/database/createDDL.sql
@@ -105,7 +105,7 @@ CREATE TABLE _meta (
   PRIMARY KEY (`version`)
 ) ;
 
-INSERT INTO _meta (version, studio_id) VALUES ('3.1.9.3', UUID()) ;
+INSERT INTO _meta (version, studio_id) VALUES ('3.1.10.1', UUID()) ;
 
 CREATE TABLE IF NOT EXISTS `audit` (
   `id`                        BIGINT(20)    NOT NULL AUTO_INCREMENT,
@@ -228,6 +228,8 @@ CREATE TABLE IF NOT EXISTS `site` (
   `sandbox_branch`                  VARCHAR(255)  NOT NULL DEFAULT 'master',
   `search_engine`                   VARCHAR(20)   NOT NULL DEFAULT 'Elasticsearch',
   `published_repo_created`          INT           NOT NULL DEFAULT 0,
+  `publishing_lock_owner`           VARCHAR(255)  NULL,
+  `publishing_lock_heartbeat`       DATETIME      NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_unique` (`id` ASC),
   UNIQUE INDEX `site_uuid_site_id_unique` (`site_uuid` ASC, `site_id` ASC),
