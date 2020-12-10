8fd14e1469a1d03f4aec2106dd099bc5901ca535
Jose Ross <jdross17@gmail.com>
2020-12-10 15:56:37 -0600

    Fix for webdav: - Add optional support for preemptive authentication

--- src/main/resources/crafter/studio/database/createDDL.sql
+++ src/main/resources/crafter/studio/database/createDDL.sql
@@ -124,7 +124,7 @@ CREATE TABLE _meta (
   PRIMARY KEY (`version`)
 ) ;
 
-INSERT INTO _meta (version, studio_id) VALUES ('3.1.11.4', UUID()) ;
+INSERT INTO _meta (version, studio_id) VALUES ('3.1.12', UUID()) ;
 
 CREATE TABLE IF NOT EXISTS `audit` (
   `id`                        BIGINT(20)    NOT NULL AUTO_INCREMENT,
