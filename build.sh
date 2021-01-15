9b7b9fb05ad8b96037c84f1178af82352e8e9df2
Sumer Jabri <sumer.jabri@craftercms.org>
2021-01-15 11:08:23 -0500

    Updated Travis CI.

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-  mvn -q -Dmaven.javadoc.skip=true clean deploy -DcloneUi=true --settings .travis-settings.xml;
-fi
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-  mvn -q -Dmaven.javadoc.skip=true clean install -DcloneUi=true;
-fi
