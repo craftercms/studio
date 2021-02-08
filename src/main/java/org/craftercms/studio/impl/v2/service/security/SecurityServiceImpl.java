b62572e266f8bc638053a2108896d1bb7f8cecbd
Dejan Brkic <dejan.brkic@craftersoftware.com>
2021-02-08 18:36:25 +0100

    Codacy fixes

--- src/main/java/org/craftercms/studio/impl/v2/service/security/SecurityServiceImpl.java
+++ src/main/java/org/craftercms/studio/impl/v2/service/security/SecurityServiceImpl.java
@@ -35,10 +35,8 @@ import org.dom4j.Node;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
