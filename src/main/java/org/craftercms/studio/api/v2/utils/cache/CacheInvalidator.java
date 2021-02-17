bb576bc21c20a5b6bc673df71a384bd0fd2417be
Jose Ross <jdross17@gmail.com>
2021-02-17 16:28:59 -0600

    Improvements for cache invalidation

---
+++ src/main/java/org/craftercms/studio/api/v2/utils/cache/CacheInvalidator.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2007-2021 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.craftercms.studio.api.v2.utils.cache;
+
+import com.google.common.cache.Cache;
+
+/**
+ * Defines the operations for cache invalidation
+ *
+ * @author joseross
+ * @since 4.0
+ * @param <K> the type for the keys
+ * @param <V> the type for the values
+ */
+public interface CacheInvalidator<K,V> {
+
+    /**
+     * Performs the cache invalidation based on the given key
+     * @param cache the cache instance
+     * @param key the key to invalidate
+     */
+    void invalidate(Cache<K, V> cache,  K key);
+
+}
