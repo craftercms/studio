e3780cb360efa57db31287270caa4245024ac8df
Jose Ross <jdross17@gmail.com>
2021-03-01 16:43:32 -0600

    Add new ui config file

--- src/main/resources/crafter/studio/database/createDDL.sql
+++ src/main/resources/crafter/studio/database/createDDL.sql
@@ -216,7 +216,7 @@ CREATE TABLE _meta (
   PRIMARY KEY (`version`)
 ) ;
 
-INSERT INTO _meta (version, studio_id) VALUES ('4.0.0.2', UUID()) ;
+INSERT INTO _meta (version, studio_id) VALUES ('4.0.0.5', UUID()) ;
 
 CREATE TABLE IF NOT EXISTS `audit` (
   `id`                        BIGINT(20)    NOT NULL AUTO_INCREMENT,
