cc18ecbc3e288a424f2d9c80c5ba651654054f0c
Dejan Brkic <dejan.brkic@craftersoftware.com>
2021-03-24 18:42:49 +0100

    Updated API docs

---
+++ src/main/java/org/craftercms/studio/api/v2/dal/PublishStatus.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2007-2021 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.craftercms.studio.api.v2.dal;
+
+public class PublishStatus {
+
+    private String status;
+    private String message;
+    private String lockOwner;
+    private String lockTTL;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getLockOwner() {
+        return lockOwner;
+    }
+
+    public void setLockOwner(String lockOwner) {
+        this.lockOwner = lockOwner;
+    }
+
+    public String getLockTTL() {
+        return lockTTL;
+    }
+
+    public void setLockTTL(String lockTTL) {
+        this.lockTTL = lockTTL;
+    }
+}
