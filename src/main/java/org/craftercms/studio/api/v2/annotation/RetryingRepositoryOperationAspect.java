0d03835e8118f9f4a80fd0010d7ebfef47bbc258
Dejan Brkic <dejan.brkic@craftersoftware.com>
2021-06-22 22:35:50 +0200

    codacy fixes

--- src/main/java/org/craftercms/studio/api/v2/annotation/RetryingRepositoryOperationAspect.java
+++ src/main/java/org/craftercms/studio/api/v2/annotation/RetryingRepositoryOperationAspect.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2020 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2021 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -27,7 +27,6 @@ import org.eclipse.jgit.api.errors.JGitInternalException;
 import org.eclipse.jgit.errors.LockFailedException;
 
 import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.Objects;
 
 @Aspect
