ad109ae050ad78533511b34214dad5f5e4969f33
Dejan Brkic <dejan.brkic@craftersoftware.com>
2021-12-10 19:13:43 +0100

    Renamed owned_by column to locked_by

--- src/main/resources/crafter/studio/database/upgrade-4.0.0.33-to-4.0.0.34.sql
+++ src/main/resources/crafter/studio/database/upgrade-4.0.0.33-to-4.0.0.34.sql
@@ -14,10 +14,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-ALTER TABLE `item` DROP CONSTRAINT `item_ix_owned_by` ;
+ALTER TABLE `item` DROP FOREIGN KEY IF EXISTS `item_ix_owned_by` ;
 
 ALTER TABLE `item` CHANGE COLUMN `owned_by` `locked_by` BIGINT NULL ;
 
-ALTER TABLE `item` ADD FOREIGN KEY item_ix_locked_by(`locked_by`) REFERENCES `user` (`id`) ;
+ALTER TABLE `item` ADD FOREIGN KEY IF NOT EXISTS item_ix_locked_by(`locked_by`) REFERENCES `user` (`id`) ;
 
 UPDATE _meta SET version = '4.0.0.34' ;
