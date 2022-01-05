f742a7dc4c1985ae205ff1dbbc5d9e0e4c6f0dfb
Dejan Brkic <dejan.brkic@craftersoftware.com>
2022-01-05 15:56:53 +0100

    Added browser url calculation for templates

--- src/main/java/org/craftercms/studio/impl/v2/service/item/internal/ItemServiceInternalImpl.java
+++ src/main/java/org/craftercms/studio/impl/v2/service/item/internal/ItemServiceInternalImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2021 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -392,7 +392,9 @@ public class ItemServiceInternalImpl implements ItemServiceInternal {
     public String getBrowserUrl(String site, String path) {
         String replacePattern;
         boolean isPage = false;
-        if (ContentUtils.matchesPatterns(path, Arrays.asList(CONTENT_TYPE_TAXONOMY_REGEX))) {
+        if (ContentUtils.matchesPatterns(path, servicesConfig.getRenderingTemplatePatterns(site))) {
+            return null;
+        } else if (ContentUtils.matchesPatterns(path, Arrays.asList(CONTENT_TYPE_TAXONOMY_REGEX))) {
             return null;
         } else if (ContentUtils.matchesPatterns(path, servicesConfig.getComponentPatterns(site)) ||
                 StringUtils.endsWith(path,FILE_SEPARATOR + servicesConfig.getLevelDescriptorName(site))) {
