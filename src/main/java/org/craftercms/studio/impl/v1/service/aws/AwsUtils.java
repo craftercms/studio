85eedd5e2113ea8e8b1bc4be077cc85979c649e2
Jose Ross <jdross17@gmail.com>
2022-01-12 13:51:07 -0600

    Fix for S3: (#2825)
    * Fix for S3:
    - Support copy operation for large files
    
    * Updates according to feedback

--- src/main/java/org/craftercms/studio/impl/v1/service/aws/AwsUtils.java
+++ src/main/java/org/craftercms/studio/impl/v1/service/aws/AwsUtils.java
@@ -29,11 +29,17 @@ import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 public abstract class AwsUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(AwsUtils.class);
 
     public static final int MIN_PART_SIZE = 5 * 1024 * 1024;
+
+    public static final int COPY_PART_SIZE = 1024 * 1024 * 1024;
+
+    public static final long MAX_COPY_FILE_SIZE = 5L * 1024 * 1024 * 1024;
 
     public static void uploadStream(String inputBucket, String inputKey, AmazonS3 s3Client, int partSize,
                                     String filename, InputStream content) throws AwsException {
@@ -94,6 +100,65 @@ public abstract class AwsUtils {
                         initResult.getUploadId()));
             }
             throw new AwsException("Upload of file '" + filename + "' failed", e);
+        }
+    }
+
+    public static void copyFile(String sourceBucket, String sourceKey, String destBucket, String destKey,
+                                int partSize, AmazonS3 client) {
+        logger.debug("Copying file from {}/{} to {}/{}", sourceBucket, sourceKey, destBucket, destKey);
+        GetObjectMetadataRequest metadataRequest = new GetObjectMetadataRequest(sourceBucket, sourceKey);
+        ObjectMetadata metadataResult = client.getObjectMetadata(metadataRequest);
+        long objectSize = metadataResult.getContentLength();
+
+        if (objectSize >= MAX_COPY_FILE_SIZE) {
+            logger.debug("Starting multipart copy for {}/{}", sourceBucket, sourceKey);
+            InitiateMultipartUploadRequest initRequest = new InitiateMultipartUploadRequest(destBucket, destKey);
+            InitiateMultipartUploadResult initResult = client.initiateMultipartUpload(initRequest);
+
+            long bytePosition = 0;
+            int partNum = 1;
+            List<CopyPartResult> copyResponses = new LinkedList<>();
+
+            try {
+                while (bytePosition < objectSize) {
+                    logger.debug("Copying part {} for {}/{}", partNum, sourceBucket, sourceKey);
+                    long lastByte = Math.min(bytePosition + partSize - 1, objectSize - 1);
+
+                    CopyPartRequest copyRequest = new CopyPartRequest()
+                            .withSourceBucketName(sourceBucket)
+                            .withSourceKey(sourceKey)
+                            .withDestinationBucketName(destBucket)
+                            .withDestinationKey(destKey)
+                            .withUploadId(initResult.getUploadId())
+                            .withFirstByte(bytePosition)
+                            .withLastByte(lastByte)
+                            .withPartNumber(partNum++);
+
+                    copyResponses.add(client.copyPart(copyRequest));
+                    bytePosition += partSize;
+                }
+
+                List<PartETag> etags = copyResponses.stream()
+                        .map(r -> new PartETag(r.getPartNumber(), r.getETag()))
+                        .collect(toList());
+
+                CompleteMultipartUploadRequest completeRequest =
+                        new CompleteMultipartUploadRequest(destBucket, destKey, initResult.getUploadId(), etags);
+                client.completeMultipartUpload(completeRequest);
+                logger.debug("Completed multipart copy for {}/{}", sourceBucket, sourceKey);
+            } catch (Exception e) {
+                if (initResult != null) {
+                    client.abortMultipartUpload(
+                            new AbortMultipartUploadRequest(destBucket, destKey, initResult.getUploadId()));
+                }
+                throw e;
+            }
+
+
+        } else {
+            logger.debug("Starting copy operation for {}/{}", sourceBucket, sourceKey);
+            client.copyObject(sourceBucket, sourceKey, destBucket, destKey);
+            logger.debug("Completed copy for {}/{}", sourceBucket, sourceKey);
         }
     }
 
