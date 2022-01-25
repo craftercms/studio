8b4055b86971741554edc9fd3e88c52dcdd7e858
Jose Ross <jdross17@gmail.com>
2022-01-25 09:22:57 -0600

    Fix format for dates on copy (#2850)

--- src/main/java/org/craftercms/studio/impl/v1/service/content/ContentServiceImpl.java
+++ src/main/java/org/craftercms/studio/impl/v1/service/content/ContentServiceImpl.java
@@ -17,7 +17,7 @@ package org.craftercms.studio.impl.v1.service.content;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.text.SimpleDateFormat;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -131,7 +131,6 @@ import static org.craftercms.studio.api.v1.constant.StudioConstants.CONTENT_TYPE
 import static org.craftercms.studio.api.v1.constant.StudioConstants.CONTENT_TYPE_TAXONOMY;
 import static org.craftercms.studio.api.v1.constant.StudioConstants.CONTENT_TYPE_TAXONOMY_REGEX;
 import static org.craftercms.studio.api.v1.constant.StudioConstants.CONTENT_TYPE_UNKNOWN;
-import static org.craftercms.studio.api.v1.constant.StudioConstants.DATE_PATTERN_WORKFLOW_WITH_TZ;
 import static org.craftercms.studio.api.v1.constant.StudioConstants.FILE_SEPARATOR;
 import static org.craftercms.studio.api.v1.ebus.EBusConstants.EVENT_PREVIEW_SYNC;
 import static org.craftercms.studio.api.v1.service.objectstate.TransitionEvent.REVERT;
@@ -1420,9 +1419,8 @@ public class ContentServiceImpl implements ContentService {
             }
         }
 
-        ZonedDateTime now = ZonedDateTime.now();
-        SimpleDateFormat sdf = new SimpleDateFormat(DATE_PATTERN_WORKFLOW_WITH_TZ);
-        String nowFormatted = now.format(DateTimeFormatter.ofPattern(sdf.toPattern()));
+        ZonedDateTime now = ZonedDateTime.now(ZoneOffset.UTC);
+        String nowFormatted = now.format(DateTimeFormatter.ISO_INSTANT);
 
         Node createdDateNode = root.selectSingleNode("//" + ELM_CREATED_DATE);
         if (createdDateNode != null) {
