e17890ee8960e459903d605177ffcbc2e5b079fb
Alfonso Vasquez <alfonso.vasquez@craftersoftware.com>
2022-03-23 15:10:31 -0400

    Fixed identation

--- src/main/java/org/craftercms/studio/impl/v2/utils/git/GitCli.java
+++ src/main/java/org/craftercms/studio/impl/v2/utils/git/GitCli.java
@@ -46,133 +46,133 @@ import org.slf4j.LoggerFactory;
  */
 public class GitCli {
 
-	private static final Logger logger = LoggerFactory.getLogger(GitCli.class);
+    private static final Logger logger = LoggerFactory.getLogger(GitCli.class);
 
-	private static final String DEFAULT_GIT_COMMAND_NAME = "git";
-	private static final int DEFAULT_GIT_PROC_WAIT_FOR_TIMEOUT = 60 * 5; // 5 mins
+    private static final String DEFAULT_GIT_COMMAND_NAME = "git";
+    private static final int DEFAULT_GIT_PROC_WAIT_FOR_TIMEOUT = 60 * 5; // 5 mins
 
-	// Exception resolvers
-	public final GitCliOutputExceptionResolver DEFAULT_EX_RESOLVER = RepositoryLockedExceptionResolver.INSTANCE;
-	public final GitCliOutputExceptionResolver COMMIT_EX_RESOLVER = new CompositeGitCliExceptionResolver(
-			RepositoryLockedExceptionResolver.INSTANCE, NoChangesToCommitExceptionResolver.INSTANCE);
+    // Exception resolvers
+    public final GitCliOutputExceptionResolver DEFAULT_EX_RESOLVER = RepositoryLockedExceptionResolver.INSTANCE;
+    public final GitCliOutputExceptionResolver COMMIT_EX_RESOLVER = new CompositeGitCliExceptionResolver(
+            RepositoryLockedExceptionResolver.INSTANCE, NoChangesToCommitExceptionResolver.INSTANCE);
 
-	private String gitProcName;
-	private int gitProcWaitForTimeoutSecs;
+    private String gitProcName;
+    private int gitProcWaitForTimeoutSecs;
 
-	public GitCli() {
-		this.gitProcName = DEFAULT_GIT_COMMAND_NAME;
-		this.gitProcWaitForTimeoutSecs = DEFAULT_GIT_PROC_WAIT_FOR_TIMEOUT;
-	}
+    public GitCli() {
+        this.gitProcName = DEFAULT_GIT_COMMAND_NAME;
+        this.gitProcWaitForTimeoutSecs = DEFAULT_GIT_PROC_WAIT_FOR_TIMEOUT;
+    }
 
-	public GitCli(String gitProcName, int gitProcWaitForTimeoutSecs) {
-		this.gitProcName = gitProcName;
-		this.gitProcWaitForTimeoutSecs = gitProcWaitForTimeoutSecs;
-	}
+    public GitCli(String gitProcName, int gitProcWaitForTimeoutSecs) {
+        this.gitProcName = gitProcName;
+        this.gitProcWaitForTimeoutSecs = gitProcWaitForTimeoutSecs;
+    }
 
-	protected String executeGitCommand(String directory, GitCommandLine commandLine)
-			throws IOException, InterruptedException {
-		return executeGitCommand(directory, commandLine, DEFAULT_EX_RESOLVER);
-	}
+    protected String executeGitCommand(String directory, GitCommandLine commandLine)
+            throws IOException, InterruptedException {
+        return executeGitCommand(directory, commandLine, DEFAULT_EX_RESOLVER);
+    }
 
-	protected String executeGitCommand(String directory, GitCommandLine commandLine,
-									   GitCliOutputExceptionResolver exceptionResolver)
-			throws IOException, InterruptedException {
-		if (Files.notExists(Paths.get(directory)) || Files.notExists(Paths.get(directory, ".git"))) {
-			throw new IOException("Directory " + directory + " does not exist or is not a Git repository");
-		}
+    protected String executeGitCommand(String directory, GitCommandLine commandLine,
+                                       GitCliOutputExceptionResolver exceptionResolver)
+            throws IOException, InterruptedException {
+        if (Files.notExists(Paths.get(directory)) || Files.notExists(Paths.get(directory, ".git"))) {
+            throw new IOException("Directory " + directory + " does not exist or is not a Git repository");
+        }
 
-		ProcessBuilder pb = new ProcessBuilder(commandLine);
-		pb.directory(new File(directory));
-		// Read stderr from stdout
-		pb.redirectErrorStream(true);
+        ProcessBuilder pb = new ProcessBuilder(commandLine);
+        pb.directory(new File(directory));
+        // Read stderr from stdout
+        pb.redirectErrorStream(true);
 
-		logger.debug("Executing git command: {}", commandLine);
+        logger.debug("Executing git command: {}", commandLine);
 
-		// Start process
-		Process p = pb.start();
+        // Start process
+        Process p = pb.start();
 
-		// Read output before wait to avoid process from hanging
-		String output = IOUtils.toString(p.getInputStream());
+        // Read output before wait to avoid process from hanging
+        String output = IOUtils.toString(p.getInputStream());
 
-		// Wait till the process has finished
-		boolean exited = p.waitFor(gitProcWaitForTimeoutSecs, TimeUnit.SECONDS);
-		if (exited) {
-			int exitValue = p.exitValue();
-			if (exitValue == 0) {
-				logger.debug("Git command successfully executed on {}:\n{}", directory, output);
+        // Wait till the process has finished
+        boolean exited = p.waitFor(gitProcWaitForTimeoutSecs, TimeUnit.SECONDS);
+        if (exited) {
+            int exitValue = p.exitValue();
+            if (exitValue == 0) {
+                logger.debug("Git command successfully executed on {}:\n{}", directory, output);
 
-				return output;
-			} else {
-				logger.debug("Git command failed with exit value {} on {}:\n{}", exitValue, directory, output);
+                return output;
+            } else {
+                logger.debug("Git command failed with exit value {} on {}:\n{}", exitValue, directory, output);
 
-				GitCliOutputException ex = exceptionResolver.resolveException(exitValue, output);
-				if (ex == null) {
-					ex = new GitCliOutputException(exitValue, output);
-				}
+                GitCliOutputException ex = exceptionResolver.resolveException(exitValue, output);
+                if (ex == null) {
+                    ex = new GitCliOutputException(exitValue, output);
+                }
 
-				throw ex;
-			}
-		} else {
-			throw new IOException("Timeout while waiting for git command to exit");
-		}
-	}
+                throw ex;
+            }
+        } else {
+            throw new IOException("Timeout while waiting for git command to exit");
+        }
+    }
 
-	public void add(String directory, String... paths) throws GitCliException {
-		try {
-			executeGitCommand(directory, new GitCommandLine("add", paths));
-		} catch (Exception e) {
-			throw new GitCliException("Git add failed on directory " + directory + " for paths " +
-									  ArrayUtils.toString(paths), e);
-		}
-	}
+    public void add(String directory, String... paths) throws GitCliException {
+        try {
+            executeGitCommand(directory, new GitCommandLine("add", paths));
+        } catch (Exception e) {
+            throw new GitCliException("Git add failed on directory " + directory + " for paths " +
+                                      ArrayUtils.toString(paths), e);
+        }
+    }
 
-	public String commit(String directory, String author, String message, String... paths) throws GitCliException {
-		GitCommandLine commitCl = new GitCommandLine("commit");
-		GitCommandLine revParseCl = new GitCommandLine("rev-parse", "HEAD");
+    public String commit(String directory, String author, String message, String... paths) throws GitCliException {
+        GitCommandLine commitCl = new GitCommandLine("commit");
+        GitCommandLine revParseCl = new GitCommandLine("rev-parse", "HEAD");
 
-		commitCl.addOption("--author", author);
-		commitCl.addOption("--message", message);
-		commitCl.addParams(paths);
+        commitCl.addOption("--author", author);
+        commitCl.addOption("--message", message);
+        commitCl.addParams(paths);
 
-		try {
-			executeGitCommand(directory, commitCl, COMMIT_EX_RESOLVER);
-			return StringUtils.trim(executeGitCommand(directory, revParseCl));
-		} catch (Exception e) {
-			throw new GitCliException("Git commit failed on directory " + directory + " for paths " +
-									  ArrayUtils.toString(paths), e);
-		}
-	}
+        try {
+            executeGitCommand(directory, commitCl, COMMIT_EX_RESOLVER);
+            return StringUtils.trim(executeGitCommand(directory, revParseCl));
+        } catch (Exception e) {
+            throw new GitCliException("Git commit failed on directory " + directory + " for paths " +
+                                      ArrayUtils.toString(paths), e);
+        }
+    }
 
-	protected class GitCommandLine extends ArrayList<String> {
+    protected class GitCommandLine extends ArrayList<String> {
 
-		public GitCommandLine(String command) {
-			add(gitProcName);
-			add(command);
-		}
+        public GitCommandLine(String command) {
+            add(gitProcName);
+            add(command);
+        }
 
-		public GitCommandLine(String command, String... params) {
-			add(gitProcName);
-			add(command);
-			addParams(params);
-		}
+        public GitCommandLine(String command, String... params) {
+            add(gitProcName);
+            add(command);
+            addParams(params);
+        }
 
-		public void addParam(String param) {
-			add(param);
-		}
+        public void addParam(String param) {
+            add(param);
+        }
 
-		public void addParams(String... params) {
-			if (ArrayUtils.isNotEmpty(params)) {
-				for (String arg : params) {
-					addParam(arg);
-				}
-			}
-		}
+        public void addParams(String... params) {
+            if (ArrayUtils.isNotEmpty(params)) {
+                for (String arg : params) {
+                    addParam(arg);
+                }
+            }
+        }
 
-		public void addOption(String optName, String optValue) {
-			addParam(optName);
-			addParam("\"" + optValue + "\"");
-		}
+        public void addOption(String optName, String optValue) {
+            addParam(optName);
+            addParam("\"" + optValue + "\"");
+        }
 
-	}
+    }
 
 }
