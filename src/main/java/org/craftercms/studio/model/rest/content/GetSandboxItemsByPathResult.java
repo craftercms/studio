933d304c5b5b639f1c2d854fe38703a43491af3b
Jose Ross <jdross17@gmail.com>
2022-05-04 12:27:03 -0600

    Fix for getSandboxItemsByPath (#2971)

---
+++ src/main/java/org/craftercms/studio/model/rest/content/GetSandboxItemsByPathResult.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+package org.craftercms.studio.model.rest.content;
+
+import org.craftercms.studio.model.rest.ResultList;
+
+import java.util.Collection;
+
+/**
+ * Holds the data returned by the getSandboxItemsByPath operation
+ *
+ * @author joseross
+ * @since 4.0.0
+ */
+public class GetSandboxItemsByPathResult extends ResultList<SandboxItem> {
+
+    /**
+     * Collection of paths for which no item was found
+     */
+    protected Collection<String> missingItems;
+
+    public Collection<String> getMissingItems() {
+        return missingItems;
+    }
+
+    public void setMissingItems(Collection<String> missingItems) {
+        this.missingItems = missingItems;
+    }
+
+}
