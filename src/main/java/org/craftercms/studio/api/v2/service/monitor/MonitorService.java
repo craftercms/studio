5d628ee208e70220229b2b368ee91d62c11d6bd1
Jonathan Mendez <jonnathan.mendez@gmail.com>
2022-07-01 11:20:50 -0600

    Tweaks to MonitorService. Adding ProxyService to secure /monitoring/log requests. Adding log_monitor permission to developer role

--- src/main/java/org/craftercms/studio/api/v2/service/monitor/MonitorService.java
+++ src/main/java/org/craftercms/studio/api/v2/service/monitor/MonitorService.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.craftercms.studio.api.v2.service.monitor;
 
 import java.util.List;
@@ -12,9 +27,8 @@ import java.util.Map;
 public interface MonitorService {
 
     /**
-     * Retrieves a list of events logged since the timestamp indicated by {@code since} parameter
+     * Retrieves a list of global events logged since the timestamp indicated by {@code since} parameter
      *
-     * @param siteId the crafter site ID
      * @param since  timestamp. Events before this value will not be included.
      * @return a list of log events
      */
--- src/main/java/org/craftercms/studio/impl/v2/service/monitor/MonitorServiceImpl.java
+++ src/main/java/org/craftercms/studio/impl/v2/service/monitor/MonitorServiceImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.craftercms.studio.impl.v2.service.monitor;
 
 import org.craftercms.commons.security.permissions.DefaultPermission;
@@ -18,7 +33,7 @@ import static org.craftercms.studio.permissions.StudioPermissionsConstants.PERMI
 public class MonitorServiceImpl implements MonitorService {
     @Override
     @HasPermission(type = DefaultPermission.class, action = PERMISSION_LOG_MONITOR)
-    public List<Map<String, Object>> getLogEvents(String siteId, long since) {
+    public List<Map<String, Object>> getLogEvents(final String siteId, final long since) {
         return CircularQueueLogAppender.getLoggedEvents(siteId, since);
     }
 }
