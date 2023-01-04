858599b3830fbc6f3c2c2b674ede6fd0bbd30c2c
Jonathan Mendez <jonnathan.mendez@gmail.com>
2023-01-04 12:12:13 -0600

    Remove unused property

--- src/main/java/org/craftercms/studio/controller/rest/v2/ContentController.java
+++ src/main/java/org/craftercms/studio/controller/rest/v2/ContentController.java
@@ -68,20 +68,16 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class ContentController {
 
     private final ContentService contentService;
-    private final SiteService siteService;
     private final DependencyService dependencyService;
     private final WorkflowService workflowService;
 
     //TODO: Migrate logic to new content service
     private final ClipboardService clipboardService;
 
-    @ConstructorProperties({"contentService", "siteService", "dependencyService", "clipboardService",
-            "workflowService"})
-    public ContentController(ContentService contentService, SiteService siteService,
-                             DependencyService dependencyService, ClipboardService clipboardService,
+    @ConstructorProperties({"contentService", "dependencyService", "clipboardService", "workflowService"})
+    public ContentController(ContentService contentService, DependencyService dependencyService, ClipboardService clipboardService,
                              WorkflowService workflowService) {
         this.contentService = contentService;
-        this.siteService = siteService;
         this.dependencyService = dependencyService;
         this.clipboardService = clipboardService;
         this.workflowService = workflowService;
