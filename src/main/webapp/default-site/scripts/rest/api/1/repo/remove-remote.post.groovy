f7af1f4c21d32353ce681b6895d4725361367097
Phil Nguyen <nquangphuong@gmail.com>
2023-01-16 14:06:04 +0700

    Retire deprecated repo's APIs

--- src/main/webapp/default-site/scripts/rest/api/1/repo/remove-remote.post.groovy
+++ src/main/webapp/default-site/scripts/rest/api/1/repo/remove-remote.post.groovy
@@ -14,70 +14,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
-import groovy.json.JsonException
-import groovy.json.JsonSlurper
-import org.apache.commons.lang3.StringUtils
-import org.craftercms.studio.api.v1.exception.SiteNotFoundException
-import scripts.api.SiteServices
-
 def result = [:]
-try {
-    def requestBody = request.reader.text
 
-    def slurper = new JsonSlurper()
-    def parsedReq = slurper.parseText(requestBody)
+result.message = "API deprecated."
+response.setStatus(410)
 
-    def siteId = parsedReq.site_id
-    def remoteName = parsedReq.remote_name
-
-/** Validate Parameters */
-    def invalidParams = false
-    def paramsList = []
-
-// site_id
-    try {
-        if (StringUtils.isEmpty(siteId)) {
-            invalidParams = true
-            paramsList.add("site_id")
-        }
-    } catch (Exception e) {
-        invalidParams = true
-        paramsList.add("site_id")
-    }
-
-    // remote_name
-    try {
-        if (StringUtils.isEmpty(remoteName)) {
-            invalidParams = true
-            paramsList.add("remote_name")
-        }
-    } catch (Exception e) {
-        invalidParams = true
-        paramsList.add("remote_name")
-    }
-
-
-    if (invalidParams) {
-        response.setStatus(400)
-        result.message = "Invalid parameter(s): " + paramsList
-    } else {
-        def context = SiteServices.createContext(applicationContext, request)
-
-        try {
-            SiteServices.removeRemote(context, siteId, remoteName)
-            response.setStatus(200)
-            result.message = "OK"
-        } catch (SiteNotFoundException e) {
-            response.setStatus(404)
-            result.message = "Site not found"
-        } catch (Exception e) {
-            response.setStatus(500)
-            result.message = "Internal server error: \n" + e
-        }
-    }
-} catch (JsonException e) {
-    response.setStatus(400)
-    result.message = "Bad Request"
-}
 return result
