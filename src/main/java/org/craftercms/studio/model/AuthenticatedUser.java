60123d133402c7341073868a9d215eaf1199591d
Phil Nguyen <nquangphuong@gmail.com>
2023-07-25 22:22:29 +0700

    Remove password field from AuthenticatedUser object (#3289)

--- src/main/java/org/craftercms/studio/model/AuthenticatedUser.java
+++ src/main/java/org/craftercms/studio/model/AuthenticatedUser.java
@@ -33,7 +33,6 @@ public class AuthenticatedUser extends User {
     public AuthenticatedUser(User user) {
         setId(user.getId());
         setUsername(user.getUsername());
-        setPassword(user.getPassword());
         setEmail(user.getEmail());
         setFirstName(user.getFirstName());
         setLastName(user.getLastName());
