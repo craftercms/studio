f2e66c9c072fa9b6c7a5ce35d96556e0580f31ed
Jonathan Mendez <jonnathan.mendez@gmail.com>
2024-01-08 22:17:22 -0600

    Get content updates when requesting directory

--- src/main/java/org/craftercms/studio/impl/v1/repository/git/GitContentRepository.java
+++ src/main/java/org/craftercms/studio/impl/v1/repository/git/GitContentRepository.java
@@ -184,8 +184,9 @@ public class GitContentRepository implements ContentRepository, ServletContextAw
                 if (tw != null && tw.getObjectId(0) != null) {
                     ObjectId id = tw.getObjectId(0);
                     ObjectLoader objectLoader = repo.open(id);
-                    toReturn = objectLoader.openStream();
-                    tw.close();
+                    if (OBJ_BLOB == objectLoader.getType()) {
+                        toReturn = objectLoader.openStream();
+                    }
                 }
             }
         } catch (IOException e) {
