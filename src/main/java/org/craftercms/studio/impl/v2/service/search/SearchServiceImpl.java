08e7a56c7c076c52615914a7e1d5813dc8c73757
Jonathan Méndez <jonnathan.mendez@gmail.com>
2024-02-19 19:12:50 -0600

    Bugfix/786 e42x (#3487)
    * Handle TooManyNestedClausesSearchException
    
    * Add logging when 'max nested clauses' limit is hit

--- src/main/java/org/craftercms/studio/impl/v2/service/search/SearchServiceImpl.java
+++ src/main/java/org/craftercms/studio/impl/v2/service/search/SearchServiceImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2023 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2024 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -19,20 +19,15 @@ package org.craftercms.studio.impl.v2.service.search;
 import org.craftercms.commons.security.permissions.DefaultPermission;
 import org.craftercms.commons.security.permissions.annotations.HasPermission;
 import org.craftercms.studio.api.v1.exception.ServiceLayerException;
-import org.craftercms.studio.api.v1.exception.security.AuthenticationException;
 import org.craftercms.studio.api.v1.service.security.SecurityService;
 import org.craftercms.studio.api.v1.service.site.SiteService;
 import org.craftercms.studio.api.v2.annotation.RequireSiteReady;
 import org.craftercms.studio.api.v2.annotation.SiteId;
 import org.craftercms.studio.api.v2.service.search.SearchService;
-import org.craftercms.studio.api.v2.service.search.internal.SearchServiceInternal;
 import org.craftercms.studio.model.search.SearchParams;
 import org.craftercms.studio.model.search.SearchResult;
 
 import java.beans.ConstructorProperties;
-import java.util.List;
-
-import static java.util.Collections.emptyList;
 import static org.craftercms.studio.permissions.StudioPermissionsConstants.PERMISSION_CONTENT_SEARCH;
 
 /**
@@ -50,14 +45,14 @@ public class SearchServiceImpl implements SearchService {
     /**
      * The internal search service
      */
-    protected final SearchServiceInternal searchServiceInternal;
+    protected final SearchService searchService;
 
     protected final SiteService siteService;
 
-    @ConstructorProperties({"securityService", "searchServiceInternal", "siteService"})
-    public SearchServiceImpl(final SecurityService securityService, final SearchServiceInternal searchServiceInternal, final SiteService siteService) {
+    @ConstructorProperties({"securityService", "searchService", "siteService"})
+    public SearchServiceImpl(final SecurityService securityService, final SearchService searchService, final SiteService siteService) {
         this.securityService = securityService;
-        this.searchServiceInternal = searchServiceInternal;
+        this.searchService = searchService;
         this.siteService = siteService;
     }
 
@@ -67,11 +62,9 @@ public class SearchServiceImpl implements SearchService {
     @Override
     @RequireSiteReady
     @HasPermission(type = DefaultPermission.class, action = PERMISSION_CONTENT_SEARCH)
-    public SearchResult search(@SiteId final String siteId, final SearchParams params)
-            throws AuthenticationException, ServiceLayerException {
-        // TODO: Get allowed paths from the security service
-        List<String> allowedPaths = emptyList();
-        return searchServiceInternal.search(siteId, allowedPaths, params);
+    public SearchResult search(@SiteId final String siteId, final SearchParams params, int maxExpansions)
+            throws ServiceLayerException {
+        return searchService.search(siteId, params, maxExpansions);
     }
 
 }
