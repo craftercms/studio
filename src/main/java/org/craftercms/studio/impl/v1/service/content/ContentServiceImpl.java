c8001cc72c4e726c266b6249e863847fbe60b0e9
Jonathan Mendez <jonnathan.mendez@gmail.com>
2024-08-14 16:57:16 -0600

    Do not trigger SyncFromRepo event when write to global repo

--- src/main/java/org/craftercms/studio/impl/v1/service/content/ContentServiceImpl.java
+++ src/main/java/org/craftercms/studio/impl/v1/service/content/ContentServiceImpl.java
@@ -659,7 +659,7 @@ public class ContentServiceImpl implements ContentService, ApplicationContextAwa
         String commitId = _contentRepository.writeContent(siteId, path, content);
 
         result = StringUtils.isNotEmpty(commitId);
-        if (result) {
+        if (result && isNotEmpty(siteId)) {
             Site site = siteService.getSite(siteId);
             processedCommitsDao.insertCommit(site.getId(), commitId);
             applicationContext.publishEvent(new SyncFromRepoEvent(siteId));
