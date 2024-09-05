bd1404fe2100071830e17fa5f2f2c86db92a5997
Jonathan Mendez <jonnathan.mendez@gmail.com>
2024-09-05 12:30:27 -0600

    Filter by path when retrieving commit author from audit table

--- src/main/java/org/craftercms/studio/impl/v2/service/audit/internal/AuditServiceInternalImpl.java
+++ src/main/java/org/craftercms/studio/impl/v2/service/audit/internal/AuditServiceInternalImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2024 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -306,8 +306,8 @@ public class AuditServiceInternalImpl implements AuditServiceInternal {
     }
 
     @Override
-    public Person getAuthor(String commitId) {
-        return auditDao.getCommitAuthor(commitId);
+    public Person getAuthor(final String commitId, final String path) {
+        return auditDao.getCommitAuthor(commitId, path);
     }
 
     public void setAuditDao(AuditDAO auditDao) {
