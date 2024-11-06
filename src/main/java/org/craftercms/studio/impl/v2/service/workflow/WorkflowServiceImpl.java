e6d409665d69056d68bf63b2becb46486da41c0d
Jonathan Méndez <jonnathan.mendez@gmail.com>
2024-11-06 08:03:58 -0600

    Include hard dependencies on workflow approve (#3650)

--- src/main/java/org/craftercms/studio/impl/v2/service/workflow/WorkflowServiceImpl.java
+++ src/main/java/org/craftercms/studio/impl/v2/service/workflow/WorkflowServiceImpl.java
@@ -449,7 +449,7 @@ public class WorkflowServiceImpl implements WorkflowService, ApplicationContextA
                     scheduledDateIsNow = true;
                     schedule = getCurrentTime();
                 }
-                deploymentService.deploy(siteId, publishingTarget, paths, schedule, publishedBy, comment, scheduledDateIsNow);
+                deploymentService.deploy(siteId, publishingTarget, pathsToPublish, schedule, publishedBy, comment, scheduledDateIsNow);
                 // Insert audit log
                 createApproveAuditLogEntry(siteId, pathsToPublish, publishedBy, comment);
                 // Trigger event
