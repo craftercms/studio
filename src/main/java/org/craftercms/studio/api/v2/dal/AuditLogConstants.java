9a471aff44a4ae255e9955c8582dfaea7a82bf7b
Phil Nguyen <nquangphuong@gmail.com>
2024-11-18 14:04:59 +0700

    Add an audit log when an Encryption Token is created

--- src/main/java/org/craftercms/studio/api/v2/dal/AuditLogConstants.java
+++ src/main/java/org/craftercms/studio/api/v2/dal/AuditLogConstants.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2024 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -71,6 +71,7 @@ public abstract class AuditLogConstants {
     public static final String TARGET_TYPE_REMOTE_REPOSITORY = "Remote Repository";
     public static final String TARGET_TYPE_ACCESS_TOKEN = "Access Token";
     public static final String TARGET_TYPE_REFRESH_TOKEN = "Refresh Token";
+    public static final String TARGET_TYPE_ENCRYPTION_TOKEN = "Encryption Token";
     public static final String TARGET_TYPE_BLUEPRINT = "Blueprint";
     public static final String TARGET_TYPE_SOURCE_SITE = "Source";
     public static final String TARGET_TYPE_PUBLISHING_PACKAGE = "Publishing Package";
--- src/main/java/org/craftercms/studio/impl/v2/service/security/internal/EncryptionServiceInternalImpl.java
+++ src/main/java/org/craftercms/studio/impl/v2/service/security/internal/EncryptionServiceInternalImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2024 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -19,51 +19,90 @@ package org.craftercms.studio.impl.v2.service.security.internal;
 import org.apache.commons.lang.StringUtils;
 import org.craftercms.commons.crypto.CryptoException;
 import org.craftercms.commons.crypto.TextEncryptor;
+import org.craftercms.studio.api.v1.dal.SiteFeed;
+import org.craftercms.studio.api.v1.dal.SiteFeedMapper;
 import org.craftercms.studio.api.v1.exception.ServiceLayerException;
+import org.craftercms.studio.api.v2.dal.AuditLog;
 import org.craftercms.studio.api.v2.exception.InvalidParametersException;
+import org.craftercms.studio.api.v2.service.audit.internal.AuditServiceInternal;
 import org.craftercms.studio.api.v2.service.security.internal.EncryptionServiceInternal;
+import org.craftercms.studio.api.v2.utils.StudioConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.beans.ConstructorProperties;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.craftercms.studio.api.v2.dal.AuditLogConstants.OPERATION_CREATE;
+import static org.craftercms.studio.api.v2.dal.AuditLogConstants.TARGET_TYPE_ENCRYPTION_TOKEN;
+import static org.craftercms.studio.api.v2.dal.QueryParameterNames.SITE_ID;
+import static org.craftercms.studio.api.v2.utils.StudioConfiguration.CONFIGURATION_GLOBAL_SYSTEM_SITE;
 
 /**
  * @author joseross
  */
 public class EncryptionServiceInternalImpl implements EncryptionServiceInternal {
 
-    protected long delay;
+    private final static Logger logger = LoggerFactory.getLogger(EncryptionServiceInternalImpl.class);
 
-    protected int maxLength;
+    private final StudioConfiguration studioConfiguration;
+    private final AuditServiceInternal auditServiceInternal;
+    private final TextEncryptor textEncryptor;
+    private final SiteFeedMapper siteFeedMapper;
+    private final int maxLength;
+    private final long delay;
 
-    protected TextEncryptor textEncryptor;
+    @ConstructorProperties({"studioConfiguration", "auditServiceInternal", "textEncryptor", "siteFeedMapper",
+            "maxLength", "delay"})
+    public EncryptionServiceInternalImpl(StudioConfiguration studioConfiguration, AuditServiceInternal auditServiceInternal,
+                                         TextEncryptor textEncryptor, SiteFeedMapper siteFeedMapper, int maxLength, long delay) {
+        this.studioConfiguration = studioConfiguration;
+        this.auditServiceInternal = auditServiceInternal;
+        this.textEncryptor = textEncryptor;
+        this.siteFeedMapper = siteFeedMapper;
+        this.maxLength = maxLength;
+        this.delay = delay;
+    }
 
     @Override
-    public String encrypt(final String text) throws ServiceLayerException {
+    public String encrypt(final String siteId, final String text) throws ServiceLayerException {
         if (StringUtils.isEmpty(text) || text.length() > maxLength) {
             throw new InvalidParametersException("The provided text is invalid");
         }
         try {
             Thread.sleep(delay * 1000);
-            return textEncryptor.encrypt(text);
+            String encryptedToken = textEncryptor.encrypt(text);
+
+            var auth = SecurityContextHolder.getContext().getAuthentication();
+            String resolvedSiteId = StringUtils.isNotEmpty(siteId) ? siteId
+                    : studioConfiguration.getProperty(CONFIGURATION_GLOBAL_SYSTEM_SITE);
+            String targetId = UUID.randomUUID().toString();
+            createEncryptionAuditLog(resolvedSiteId, auth.getName(), targetId);
+            logger.debug("Encryption token created for site '{}', target ID '{}'", resolvedSiteId, targetId);
+
+            return encryptedToken;
         } catch (CryptoException | InterruptedException e) {
             throw new ServiceLayerException("Error encrypting text", e);
         }
     }
 
-    public long getDelay() {
-        return delay;
-    }
-
-    public void setDelay(long delay) {
-        this.delay = delay;
-    }
-
-    public int getMaxLength() {
-        return maxLength;
-    }
-
-    public void setMaxLength(int maxLength) {
-        this.maxLength = maxLength;
-    }
-
-    public void setTextEncryptor(TextEncryptor textEncryptor) {
-        this.textEncryptor = textEncryptor;
+    /**
+     * Create encryption audit log
+     * @param siteId site identifier
+     * @param actor actor
+     * @param targetId encryption target id
+     */
+    private void createEncryptionAuditLog(String siteId, String actor, String targetId) {
+        SiteFeed site = siteFeedMapper.getSite(Map.of(SITE_ID, siteId));
+        AuditLog entry = auditServiceInternal.createAuditLogEntry();
+        entry.setOperation(OPERATION_CREATE);
+        entry.setActorId(actor);
+        entry.setSiteId(site.getId());
+        entry.setPrimaryTargetId(targetId);
+        entry.setPrimaryTargetType(TARGET_TYPE_ENCRYPTION_TOKEN);
+        entry.setPrimaryTargetValue(targetId);
+        auditServiceInternal.insertAuditLog(entry);
     }
 }
