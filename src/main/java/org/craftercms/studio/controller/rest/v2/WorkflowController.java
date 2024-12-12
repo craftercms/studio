14c8275b2d97205696f6e389e4ed7d6c1d7647f7
Jonathan Mendez <jonnathan.mendez@gmail.com>
2024-12-11 21:06:54 -0600

    Remove unused workflow affected_paths API

--- src/main/java/org/craftercms/studio/controller/rest/v2/WorkflowController.java
+++ src/main/java/org/craftercms/studio/controller/rest/v2/WorkflowController.java
@@ -134,17 +134,6 @@ public class WorkflowController {
         return result;
     }
 
-    @Deprecated
-    @GetMapping(value = AFFECTED_PATHS, produces = APPLICATION_JSON_VALUE)
-    public ResultList<SandboxItem> getWorkflowAffectedPaths(@ValidSiteId @RequestParam(REQUEST_PARAM_SITEID) String siteId,
-                                                            @ValidExistingContentPath @RequestParam(REQUEST_PARAM_PATH) String path) {
-        // TODO: remove this once the UI switches to the new endpoint getWorkflowAffectedPackages
-        ResultList<SandboxItem> result = new ResultList<>();
-        result.setEntities(RESULT_KEY_ITEMS, emptyList());
-        result.setResponse(OK);
-        return result;
-    }
-
     @GetMapping(value = PATH_PARAM_SITE + AFFECTED_PACKAGES, produces = APPLICATION_JSON_VALUE)
     public ResultList<PublishPackage> getWorkflowAffectedPackages(@ValidSiteId @PathVariable String site,
                                                                   @ValidExistingContentPath @RequestParam(REQUEST_PARAM_PATH) String path,
