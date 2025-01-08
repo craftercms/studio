86da639222ac4846aee1061a2ba5101bf0ede87f
Phil Nguyen <nquangphuong@gmail.com>
2025-01-09 00:54:05 +0700

    Check if path regex is empty for update_item_states_by_query API (#3683)
    * Handle path regex for update_item_states_by_query API
    
    * Check if path is valid regex for update_item_states_by_query API

--- src/main/java/org/craftercms/studio/controller/rest/v2/WorkflowController.java
+++ src/main/java/org/craftercms/studio/controller/rest/v2/WorkflowController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2024 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2025 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -21,6 +21,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
+import org.apache.commons.lang3.StringUtils;
 import org.craftercms.commons.validation.annotations.param.ValidExistingContentPath;
 import org.craftercms.commons.validation.annotations.param.ValidSiteId;
 import org.craftercms.studio.api.v1.exception.ServiceLayerException;
@@ -45,7 +46,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import static java.util.Collections.emptyList;
 import static org.apache.commons.collections4.CollectionUtils.emptyIfNull;
 import static org.craftercms.studio.controller.rest.v2.RequestConstants.*;
 import static org.craftercms.studio.controller.rest.v2.RequestMappingConstants.*;
@@ -59,6 +59,8 @@ import static org.springframework.util.CollectionUtils.isEmpty;
 @RestController
 @RequestMapping(API_2 + WORKFLOW)
 public class WorkflowController {
+
+    public static final String DEFAULT_QUERY_BY_PATH_REGEX = ".*";
 
     private final WorkflowService workflowService;
     private final PublishService publishService;
@@ -121,10 +123,14 @@ public class WorkflowController {
 
     @PostMapping(value = UPDATE_ITEM_STATES_BY_QUERY, produces = APPLICATION_JSON_VALUE)
     public Result updateItemStatesByQuery(@Valid @RequestBody UpdateItemStatesByQueryRequestBody requestBody)
-            throws SiteNotFoundException {
+            throws SiteNotFoundException, InvalidParametersException {
         UpdateItemStatesByQueryRequestBody.Query query = requestBody.getQuery();
         ItemStatesUpdate update = requestBody.getUpdate();
-        workflowService.updateItemStatesByQuery(query.getSiteId(), query.getPath(),
+        String resolvedPathRegex = StringUtils.isNotEmpty(query.getPath()) ? query.getPath() : DEFAULT_QUERY_BY_PATH_REGEX;
+        if (!isPathRegexValid(resolvedPathRegex)) {
+            throw new InvalidParametersException("Parameter 'path' is not valid regular expression.");
+        }
+        workflowService.updateItemStatesByQuery(query.getSiteId(), resolvedPathRegex,
                 query.getStates(), update.isClearSystemProcessing(),
                 update.isClearUserLocked(), update.getLive(),
                 update.getStaged(), update.getNew(), update.getModified());
