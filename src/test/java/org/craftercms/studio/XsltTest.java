f04709e765b97b7f96ccfd12edf1ca28d2202527
Jonathan Mendez <jonnathan.mendez@gmail.com>
2025-01-17 16:45:56 -0600

    Global permissions updates

--- src/test/java/org/craftercms/studio/XsltTest.java
+++ src/test/java/org/craftercms/studio/XsltTest.java
@@ -341,10 +341,27 @@ public class XsltTest {
 		};
 	}
 
+    @DataProvider(name = "globalPermissions500TestData")
+    public Object[][] globalPermissions500TestData() {
+        return new Object[][]{
+                {
+                        new ClassPathResource("crafter/studio/upgrade/5.0.x/system/global-permission-mappings-config-v5.0.0.1.xslt"),
+                        new ClassPathResource("crafter/studio/upgrade/xslt/global-permission-mappings/5.0/5.0.0.1/input.xml"),
+                        new ClassPathResource("crafter/studio/upgrade/xslt/global-permission-mappings/5.0/5.0.0.1/expected.xml"),
+                        emptyMap()
+                }
+        };
+    }
+
 	@Test(dataProvider = "permissions500TestData")
 	public void permissions500Test(Resource template, Resource content, Resource expected, Map<String, Object> params) throws IOException, TransformerException {
 		testXsltTemplate(template, content, expected, params);
 	}
+
+    @Test(dataProvider = "globalPermissions500TestData")
+    public void globalPermissions500Test(Resource template, Resource content, Resource expected, Map<String, Object> params) throws IOException, TransformerException {
+        testXsltTemplate(template, content, expected, params);
+    }
 
 	@Test(dataProvider = "xsltData")
 	public void testXsltData(Resource template, Resource content, Resource expected, Map<String, Object> params) throws IOException, TransformerException {
