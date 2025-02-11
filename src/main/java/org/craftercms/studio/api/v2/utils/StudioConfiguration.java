d372fe73aa2eaa69b81565b052dde523ce2cb1db
Jonathan Méndez <jonnathan.mendez@gmail.com>
2025-02-11 13:21:44 -0600

    Make publish queue check job run every 60 seconds (#3721)
    * Make publish queue check job run every 60 seconds
    
    * Remove publish trigger API from spec

--- src/main/java/org/craftercms/studio/api/v2/utils/StudioConfiguration.java
+++ src/main/java/org/craftercms/studio/api/v2/utils/StudioConfiguration.java
@@ -319,10 +319,6 @@ public interface StudioConfiguration {
 	String CLOCK_JOB_TASK_EXECUTOR_CORE_POOL_SIZE = "studio.clockJob.taskExecutor.corePoolSize";
 	String CLOCK_JOB_TASK_EXECUTOR_MAX_POOL_SIZE = "studio.clockJob.taskExecutor.maxPoolSize";
 	String CLOCK_JOB_TASK_EXECUTOR_QUEUE_CAPACITY = "studio.clockJob.taskExecutor.queueCapacity";
-	String CLOCK_JOB_TASK_PUBLISHER_EXECUTE_EVERY_N_CYCLES =
-		"studio.clockJob.task.publisher.executeEveryNCycles";
-	String CLOCK_JOB_TASK_PUBLISHER_MAX_RETRY_COUNT =
-		"studio.clockJob.task.publisher.maxRetryCount";
 
 	String PLUGIN_BASE_PATTERN = "studio.configuration.plugin.base.pattern";
 
