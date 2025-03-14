3cf06c6e171493a9005c3c48530a05df8cc44d51
Jonathan Mendez <jonnathan.mendez@gmail.com>
2025-03-13 18:24:44 -0600

    Make Groovy Script engine site-aware

--- src/main/java/org/craftercms/studio/impl/v2/scripting/ScriptEngineManagerImpl.java
+++ src/main/java/org/craftercms/studio/impl/v2/scripting/ScriptEngineManagerImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2025 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -23,12 +23,12 @@ import groovy.util.ResourceException;
 import org.codehaus.groovy.control.CompilerConfiguration;
 import org.craftercms.core.service.ContentStoreService;
 import org.craftercms.engine.util.url.ContentStoreUrlConnection;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.craftercms.studio.api.v2.core.ContextManager;
 import org.craftercms.studio.api.v2.scripting.ScriptEngineManager;
 import org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.RejectASTTransformsCustomizer;
 import org.kohsuke.groovy.sandbox.SandboxTransformer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.beans.ConstructorProperties;
 import java.io.File;
@@ -38,8 +38,6 @@ import java.net.URLConnection;
 import java.net.URLStreamHandler;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
-import static org.craftercms.studio.api.v2.utils.StudioUtils.getSiteId;
 
 /**
  * Default implementation of {@link ScriptEngineManager}
@@ -92,9 +90,9 @@ public class ScriptEngineManagerImpl implements ScriptEngineManager {
 		}
 
 		var groovyClassloader = new GroovyClassLoader(getClass().getClassLoader(), compilerConfig);
-		groovyClassloader.setResourceLoader(new StudioResourceLoader(classesBasePath, new StudioUrlStreamHandler()));
+		groovyClassloader.setResourceLoader(new StudioResourceLoader(siteId, classesBasePath, new StudioUrlStreamHandler(siteId)));
 
-		return new GroovyScriptEngine(new StudioResourceConnector(restBasePath), groovyClassloader);
+		return new GroovyScriptEngine(new StudioResourceConnector(siteId, restBasePath), groovyClassloader);
 	}
 
 	@Override
@@ -107,9 +105,15 @@ public class ScriptEngineManagerImpl implements ScriptEngineManager {
 
 	protected class StudioUrlStreamHandler extends URLStreamHandler {
 
+		private final String siteId;
+
+		public StudioUrlStreamHandler(final String siteId) {
+			this.siteId = siteId;
+		}
+
 		@Override
 		protected URLConnection openConnection(URL url) {
-			var context = contextManager.getContext(getSiteId());
+			var context = contextManager.getContext(siteId);
 			return new ContentStoreUrlConnection(url, contentStoreService.getContent(context, url.getFile()));
 		}
 
@@ -117,16 +121,18 @@ public class ScriptEngineManagerImpl implements ScriptEngineManager {
 
 	protected class StudioResourceConnector implements ResourceConnector {
 
+		private final String siteId;
 		private final String basePath;
 
-		public StudioResourceConnector(String basePath) {
+		public StudioResourceConnector(final String siteId, final String basePath) {
+			this.siteId = siteId;
 			this.basePath = basePath;
 		}
 
 		@Override
 		public URLConnection getResourceConnection(String name) throws ResourceException {
 			try {
-				var context = contextManager.getContext(getSiteId());
+				var context = contextManager.getContext(siteId);
 				var path = basePath + "/" + name;
 				return new ContentStoreUrlConnection(new File(path).toURI().toURL(),
 					contentStoreService.getContent(context, path));
@@ -138,11 +144,12 @@ public class ScriptEngineManagerImpl implements ScriptEngineManager {
 
 	protected class StudioResourceLoader implements GroovyResourceLoader {
 
+		private final String siteId;
 		private final String basePath;
-
 		private final URLStreamHandler urlStreamHandler;
 
-		public StudioResourceLoader(String basePath, URLStreamHandler urlStreamHandler) {
+		public StudioResourceLoader(final String siteId, final String basePath, final URLStreamHandler urlStreamHandler) {
+			this.siteId = siteId;
 			this.basePath = basePath;
 			this.urlStreamHandler = urlStreamHandler;
 		}
@@ -156,7 +163,7 @@ public class ScriptEngineManagerImpl implements ScriptEngineManager {
 				filename += "." + scriptExtension;
 			}
 
-			var context = contextManager.getContext(getSiteId());
+			var context = contextManager.getContext(siteId);
 			var path = basePath + "/" + filename;
 			if (contentStoreService.exists(context, path)) {
 				return new URL(null, "site:" + path, urlStreamHandler);
