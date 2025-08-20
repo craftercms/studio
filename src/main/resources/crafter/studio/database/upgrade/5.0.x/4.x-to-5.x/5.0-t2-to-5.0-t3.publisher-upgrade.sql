75e283d1cc47eb26fa7d7eb8a7685b7981085ea4
Jonathan Mendez <jonnathan.mendez@gmail.com>
2025-08-20 10:19:27 -0600

    Set migrated in-processing packages to ready Publish package migration tweaks

--- src/main/resources/crafter/studio/database/upgrade/5.0.x/4.x-to-5.x/5.0-t2-to-5.0-t3.publisher-upgrade.sql
+++ src/main/resources/crafter/studio/database/upgrade/5.0.x/4.x-to-5.x/5.0-t2-to-5.0-t3.publisher-upgrade.sql
@@ -13,12 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-/*
-- populate publish_package
-- Populate publish_item
-- Populate item_publish_item for READY packages
-*/
+/************************* POPULATE  publish_package *************************/
 INSERT INTO publish_package
 			(site_id, target, title, schedule, approval_state, package_state, live_error, staging_error, submitter_id,
 			 submitter_comment, submitted_on, reviewer_id, reviewer_comment, reviewed_on, published_on, package_type, commit_id,
@@ -27,17 +22,19 @@ INSERT INTO publish_package
 		SELECT s.id, pr.environment, 'Migrated package', pr.scheduleddate, IF(w.state = 'OPENED', 'SUBMITTED', IF(pr.state = 'CANCELLED', 'REJECTED', 'APPROVED')) AS approval_state,
 				CASE pr.state
 					WHEN 'READY_FOR_LIVE' THEN POWER(2, 0) -- READY
-					WHEN 'COMPLETED' THEN POWER(2, 8) + IF(pr.environment = 'live', POWER(2, 2), POWER(2, 2) + POWER(2, 5)) -- COMPLETED + LIVE_SUCCESS + STAGING_SUCCESS
+					WHEN 'PROCESSING' THEN POWER(2, 0) -- READY
+					WHEN 'COMPLETED' THEN POWER(2, 8) + IF(pr.environment = 'live', POWER(2, 2) + POWER(2, 5), POWER(2, 5)) -- COMPLETED + LIVE_SUCCESS + STAGING_SUCCESS
 					ELSE POWER(2, 9) -- Anything else (cancelled, blocked, processing) is CANCELLED
 				END AS package_state,
-				0 AS live_error, 0 as staging_error, w.submitter_id, pr.submissioncomment, w.submitted_on, w.reviewer_id, w.reviewer_comment, NULL,
+				0 AS live_error, 0 as staging_error, IFNULL(w.submitter_id, (SELECT u.id FROM user u WHERE u.username = pr.username)) AS submitter_id,
+				pr.submissioncomment, w.submitted_on, w.reviewer_id, w.reviewer_comment, NULL,
 				pr.published_on, 'ITEM_LIST', NULL, NULL, NULL, pr.package_id
 		FROM publish_request pr
 			INNER JOIN site s ON s.site_id = pr.site
-			LEFT JOIN item i ON pr.path = i.path
-			LEFT JOIN workflow w ON w.item_id = i.id
+			LEFT JOIN workflow w ON w.publishing_package_id = pr.package_id
 		GROUP BY pr.package_id ;
 
+/************************* POPULATE  publish_item *************************/
 INSERT INTO publish_item
 			(package_id, path, live_previous_path, staging_previous_path,
 			`action`, user_requested, publish_state, live_error, staging_error)
@@ -52,11 +49,12 @@ INSERT INTO publish_item
 					ELSE 'UPDATE'
 				END AS action, true AS user_requested,
 				CASE pr.state
-					WHEN 'COMPLETED' THEN IF(pr.environment = 'live', POWER(2, 2), POWER(2, 2) + POWER(2, 4)) -- if live, live_success, otherwise live_success+staging_success
+					WHEN 'COMPLETED' THEN IF(pr.environment = 'live', POWER(2, 2) + POWER(2, 4), POWER(2, 4)) -- if live, live_success+staging_success, otherwise staging_success
 					ELSE POWER(2, 0) -- PENDING, default value
 				END AS publish_state, 0, 0
 			FROM publish_request pr ;
 
+/************************* POPULATE  item_publish_item *************************/
 INSERT INTO item_publish_item
 			(publish_item_id, item_id)
 			SELECT pi.id, i.id
