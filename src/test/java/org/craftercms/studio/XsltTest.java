f6a606f1da58c68aef7e6cc135aa7bc306dc8384
Jonathan Mendez <jonnathan.mendez@gmail.com>
2025-09-26 15:44:13 -0600

    Dependency resolver to support relative page paths

--- src/test/java/org/craftercms/studio/XsltTest.java
+++ src/test/java/org/craftercms/studio/XsltTest.java
@@ -409,6 +409,23 @@ public class XsltTest {
 		testXsltTemplate(template, content, expected, params);
 	}
 
+	@DataProvider(name = "depResolver5001TestData")
+	public Object[][] depResolver5001TestData() {
+		return new Object[][]{
+				new Object[] {
+						new ClassPathResource("crafter/studio/upgrade/5.0.x/config/resolver-config/resolver-config-v5.0.0.1.xslt"),
+						new ClassPathResource("crafter/studio/upgrade/xslt/resolver-config/5.0/5.0.0.1/input.xml"),
+						new ClassPathResource("crafter/studio/upgrade/xslt/resolver-config/5.0/5.0.0.1/expected.xml"),
+						emptyMap()
+				}
+		};
+	}
+
+	@Test(dataProvider = "depResolver5001TestData")
+	public void depResolver5001Test(Resource template, Resource content, Resource expected, Map<String, Object> params) throws IOException, TransformerException {
+		testXsltTemplate(template, content, expected, params);
+	}
+
 	private void testXsltTemplate(Resource template, Resource content, Resource expected, Map<String, Object> params)
 		throws IOException, TransformerException {
 		try (InputStream templateIs = template.getInputStream();
