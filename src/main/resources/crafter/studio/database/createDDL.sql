ded817282ac3c949953ea7c31c528e83af3d1065
Jonathan Mendez <jonnathan.mendez@gmail.com>
2025-09-29 13:22:57 -0600

    Update global dependency resolver

--- src/main/resources/crafter/studio/database/createDDL.sql
+++ src/main/resources/crafter/studio/database/createDDL.sql
@@ -225,7 +225,7 @@ CREATE TABLE _meta (
 	PRIMARY KEY (`version`)
 ) ;
 
-INSERT INTO _meta (version, studio_id) VALUES ('5.0.0.5', UUID()) ;
+INSERT INTO _meta (version, studio_id) VALUES ('5.0.0.7', UUID()) ;
 
 CREATE TABLE IF NOT EXISTS `audit` (
 	`id`                        BIGINT(20)    NOT NULL AUTO_INCREMENT,
