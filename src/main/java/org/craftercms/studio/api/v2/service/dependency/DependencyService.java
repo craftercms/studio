bd89b7ce7b0baf48c58ba370ab9c9772820f972b
Jonathan Mendez <jonnathan.mendez@gmail.com>
2025-10-07 16:46:20 -0600

    Missing @throws in javadoc

--- src/main/java/org/craftercms/studio/api/v2/service/dependency/DependencyService.java
+++ src/main/java/org/craftercms/studio/api/v2/service/dependency/DependencyService.java
@@ -97,6 +97,7 @@ public interface DependencyService {
 	 * @param siteId site identifier
 	 * @param path   path to get dependent items for
 	 * @return list of {@link LightItem} dependent on given path
+	 * @throws ContentNotFoundException if the item at the given path does not exist
 	 */
 	List<LightItem> getDependentItems(String siteId, String path) throws ContentNotFoundException;
 
