ad1ecc92173184e392fa06bcaafd347d2b6d71ba
Jonathan Mendez <jonnathan.mendez@gmail.com>
2025-10-31 16:18:09 -0600

    Upgrade to OpenSearch 3.3: Fix blueprint groovy search code

--- src/main/webapp/repo-bootstrap/global/blueprints/1000_website_editorial/scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy
+++ src/main/webapp/repo-bootstrap/global/blueprints/1000_website_editorial/scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2025 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -111,7 +111,7 @@ class SearchHelper {
 		HIGHLIGHT_FIELDS.each { field -> highlighter.fields(field, f -> f) }
 
 		SearchRequest request = SearchRequest.of(r -> r
-			.query(query.build()._toQuery())
+			.query(query.build().toQuery())
 			.from(start)
 			.size(rows)
 			.highlight(highlighter.build())
