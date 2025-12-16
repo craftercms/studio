1a0f127ef0ba603065911b85043b9c85c44356d3
Jonathan Mendez <jonnathan.mendez@gmail.com>
2025-12-16 13:29:26 -0600

    Issue with Instant in websocket messages

--- src/main/java/org/craftercms/studio/config/WebsocketConfig.java
+++ src/main/java/org/craftercms/studio/config/WebsocketConfig.java
@@ -15,15 +15,24 @@
  */
 package org.craftercms.studio.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.craftercms.studio.api.v2.utils.StudioConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.DefaultContentTypeResolver;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.security.messaging.web.csrf.CsrfChannelInterceptor;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
 
 import static org.craftercms.commons.spring.cors.FixedCorsConfigurationSource.getOrigins;
 import static org.craftercms.studio.api.v2.utils.StudioConfiguration.CONFIGURATION_CORS_ALLOWED_ORIGINS;
@@ -75,5 +84,22 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Bean
 	public CsrfChannelInterceptor csrfChannelInterceptor() {
 		return new CsrfChannelInterceptor();
+	}
+
+	@Override
+	public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+		// Configure Jackson converter with Java Time module support
+		// so that Java 8 date/time types are properly serialized/deserialized
+		DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
+		resolver.setDefaultMimeType(MimeTypeUtils.APPLICATION_JSON);
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		ObjectMapper objectMapper=  com.fasterxml.jackson.databind.json.JsonMapper.builder()
+				.addModule(new JavaTimeModule())
+				.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) // Serialize dates as formatted strings
+				.build();
+		converter.setObjectMapper(objectMapper);
+		converter.setContentTypeResolver(resolver);
+		messageConverters.add(converter);
+		return false;
 	}
 }
