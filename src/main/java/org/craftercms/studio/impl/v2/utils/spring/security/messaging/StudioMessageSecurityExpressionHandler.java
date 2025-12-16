ba8c45ad769a6f79b6c8897d89457c5f4aef11a3
Jonathan Mendez <jonnathan.mendez@gmail.com>
2025-12-15 22:21:35 -0600

    Update websocket SecurityExpressionHandler for Spring 7

--- src/main/java/org/craftercms/studio/impl/v2/utils/spring/security/messaging/StudioMessageSecurityExpressionHandler.java
+++ src/main/java/org/craftercms/studio/impl/v2/utils/spring/security/messaging/StudioMessageSecurityExpressionHandler.java
@@ -16,11 +16,19 @@
 package org.craftercms.studio.impl.v2.utils.spring.security.messaging;
 
 import org.craftercms.studio.api.v2.service.security.UserService;
+import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.Nullable;
+import org.springframework.expression.BeanResolver;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.messaging.Message;
 import org.springframework.security.access.expression.SecurityExpressionOperations;
 import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.messaging.access.expression.DefaultMessageSecurityExpressionHandler;
+import org.springframework.security.messaging.access.expression.MessageSecurityExpressionRoot;
+
+import java.util.function.Supplier;
 
 /**
  * Extension of {@link DefaultMessageSecurityExpressionHandler} that allows to integrate Studio security expressions
@@ -29,19 +37,19 @@ import org.springframework.security.messaging.access.expression.DefaultMessageSe
  * @see StudioMessageSecurityExpressionRoot
  * @since 4.0.0
  */
-public class StudioSecurityExpressionHandler<T> extends DefaultMessageSecurityExpressionHandler<T> {
+public class StudioMessageSecurityExpressionHandler<T> extends DefaultMessageSecurityExpressionHandler<T> {
 
 	protected final UserService userService;
 
-	public StudioSecurityExpressionHandler(UserService userService) {
+	public StudioMessageSecurityExpressionHandler(UserService userService) {
 		this.userService = userService;
 	}
 
+	@NotNull
 	@Override
 	protected SecurityExpressionOperations createSecurityExpressionRoot(Authentication authentication,
-																		Message<T> invocation) {
-		StudioMessageSecurityExpressionRoot root = new StudioMessageSecurityExpressionRoot(authentication, invocation,
-			userService);
+																		@NotNull Message<T> invocation) {
+		StudioMessageSecurityExpressionRoot<T> root = createSecurityExpressionRoot(() -> authentication, invocation);
 		root.setPermissionEvaluator(getPermissionEvaluator());
 		// A new instance needs to be created because it is private in the super class
 		root.setTrustResolver(new AuthenticationTrustResolverImpl());
@@ -49,4 +57,23 @@ public class StudioSecurityExpressionHandler<T> extends DefaultMessageSecurityEx
 		return root;
 	}
 
+	@NotNull
+	@Override
+	public EvaluationContext createEvaluationContext(@NotNull Supplier<? extends @Nullable Authentication> authentication, @NotNull Message<T> invocation) {
+		MessageSecurityExpressionRoot<T> root = createSecurityExpressionRoot(authentication, invocation);
+		StandardEvaluationContext ctx = new StandardEvaluationContext(root);
+		BeanResolver beanResolver = getBeanResolver();
+		if (beanResolver != null) {
+			// https://github.com/spring-projects/spring-framework/issues/35371
+			ctx.setBeanResolver(beanResolver);
+		}
+		return ctx;
+	}
+
+	private StudioMessageSecurityExpressionRoot<T> createSecurityExpressionRoot(Supplier<? extends Authentication> authentication, Message<T> invocation) {
+		StudioMessageSecurityExpressionRoot<T> root = new StudioMessageSecurityExpressionRoot<T>(authentication, invocation, userService);
+		root.setAuthorizationManagerFactory(getAuthorizationManagerFactory());
+		root.setPermissionEvaluator(getPermissionEvaluator());
+		return root;
+	}
 }
