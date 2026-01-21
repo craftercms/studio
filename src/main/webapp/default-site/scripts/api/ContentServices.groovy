0897a3770069ae10e6afac6a6b3e352210d7da56
Jonathan Mendez <jonnathan.mendez@gmail.com>
2026-01-21 16:51:00 -0600

    More API v1 cleanup

--- src/main/webapp/default-site/scripts/api/ContentServices.groovy
+++ src/main/webapp/default-site/scripts/api/ContentServices.groovy
@@ -71,11 +71,6 @@ class ContentServices {
 		return contentTypeServicesImpl.getContentType(site, type)
 	}
 
-	static getPages(context, site, path, depth, order, checkChildren = true) {
-		def contentServicesImpl = ServiceFactory.getContentServices(context);
-		return contentServicesImpl.getContentItemTree(site, path, depth);//.getPages(site, path, depth, order, checkChildren)
-	}
-
 	static reorderItems(context, site, path, before, after) {
 		def contentServicesImpl = ServiceFactory.getContentServices(context);
 		return contentServicesImpl.reorderItems(site, path, before, after);
