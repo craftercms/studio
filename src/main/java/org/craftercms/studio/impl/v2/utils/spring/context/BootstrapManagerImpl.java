e930fcdc35a3a9725d6da1eac50ab91d20c54a8b
Jonathan Mendez <jonnathan.mendez@gmail.com>
2026-05-13 11:05:11 -0600

    Restore interrupt status on InterruptedException

--- src/main/java/org/craftercms/studio/impl/v2/utils/spring/context/BootstrapManagerImpl.java
+++ src/main/java/org/craftercms/studio/impl/v2/utils/spring/context/BootstrapManagerImpl.java
@@ -88,6 +88,7 @@ public class BootstrapManagerImpl implements SystemStatusProvider, BootstrapMana
 			upgradeSemaphore.acquire();
 		} catch (InterruptedException e) {
 			logger.warn("Interrupted while waiting for system upgrade to complete, starting sites upgrade anyway", e);
+			Thread.currentThread().interrupt(); // restore interrupt status
 		}
 		logger.info("Start upgrade ...");
 		return new StartSitesUpgradeEvent(this);
