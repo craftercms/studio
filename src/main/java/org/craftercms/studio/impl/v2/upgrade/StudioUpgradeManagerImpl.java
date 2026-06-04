cc4c809e509c39eba7757668eef86947b60d8a31
Jonathan Mendez <jonnathan.mendez@gmail.com>
2026-06-04 13:44:11 -0600

    Make sites upgrade and repository cleanup run in parallel

--- src/main/java/org/craftercms/studio/impl/v2/upgrade/StudioUpgradeManagerImpl.java
+++ src/main/java/org/craftercms/studio/impl/v2/upgrade/StudioUpgradeManagerImpl.java
@@ -49,6 +49,10 @@ import javax.sql.DataSource;
 import java.beans.ConstructorProperties;
 import java.sql.SQLException;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static java.nio.file.Paths.get;
 import static java.util.stream.Collectors.toList;
@@ -85,17 +89,21 @@ public class StudioUpgradeManagerImpl extends AbstractUpgradeManager<String> imp
 	protected final InstanceService instanceService;
 	protected final RetryingRepositoryOperationFacade retryingRepositoryOperationFacade;
 	protected final SiteBootstrapStateProvider siteBootstrapStateProvider;
+	protected final int executorThreadCount;
+	protected final int executorTimeoutSeconds;
 
 	@ConstructorProperties({"dbVersionProvider", "dbPipelineFactory", "bpPipelineFactory", "configurationProvider",
-		"dataSource", "integrityValidator", "contentRepository", "studioConfiguration", "instanceService",
-		"retryingRepositoryOperationFacade", "siteBootstrapStateProvider"})
+			"dataSource", "integrityValidator", "contentRepository", "studioConfiguration", "instanceService",
+			"retryingRepositoryOperationFacade", "siteBootstrapStateProvider",
+			"executorThreadCount", "executorTimeoutSeconds"})
 	public StudioUpgradeManagerImpl(VersionProvider dbVersionProvider,
 									UpgradePipelineFactory<String> dbPipelineFactory,
 									UpgradePipelineFactory<String> bpPipelineFactory, YamlConfigurationProvider configurationProvider,
 									DataSource dataSource, DbIntegrityValidator integrityValidator,
 									GitContentRepository contentRepository, StudioConfiguration studioConfiguration,
 									InstanceService instanceService,
-									RetryingRepositoryOperationFacade retryingRepositoryOperationFacade, SiteBootstrapStateProvider siteBootstrapStateProvider) {
+									RetryingRepositoryOperationFacade retryingRepositoryOperationFacade, SiteBootstrapStateProvider siteBootstrapStateProvider,
+									int executorThreadCount, int executorTimeoutSeconds) {
 		this.dbVersionProvider = dbVersionProvider;
 		this.dbPipelineFactory = dbPipelineFactory;
 		this.bpPipelineFactory = bpPipelineFactory;
@@ -107,6 +115,8 @@ public class StudioUpgradeManagerImpl extends AbstractUpgradeManager<String> imp
 		this.instanceService = instanceService;
 		this.retryingRepositoryOperationFacade = retryingRepositoryOperationFacade;
 		this.siteBootstrapStateProvider = siteBootstrapStateProvider;
+		this.executorThreadCount = executorThreadCount;
+		this.executorTimeoutSeconds = executorTimeoutSeconds;
 	}
 
 	/**
@@ -158,7 +168,7 @@ public class StudioUpgradeManagerImpl extends AbstractUpgradeManager<String> imp
 	@Override
 	protected UpgradeContext<String> createUpgradeContext(String site) {
 		return new StudioUpgradeContext(site, studioConfiguration, dataSource, instanceService,
-			retryingRepositoryOperationFacade);
+				retryingRepositoryOperationFacade);
 	}
 
 	/**
@@ -189,7 +199,7 @@ public class StudioUpgradeManagerImpl extends AbstractUpgradeManager<String> imp
 						basePath = studioConfiguration.getProperty(CONFIGURATION_SITE_CONFIG_BASE_PATH_PATTERN);
 					} else {
 						basePath = studioConfiguration.getProperty(
-							CONFIGURATION_SITE_MUTLI_ENVIRONMENT_CONFIG_BASE_PATH_PATTERN);
+								CONFIGURATION_SITE_MUTLI_ENVIRONMENT_CONFIG_BASE_PATH_PATTERN);
 					}
 					configPath = get(replace(basePath, values, "{", "}"), file).toString();
 					logger.info("Check for upgrades to file '{}' in site '{}'", configPath, site);
@@ -213,6 +223,37 @@ public class StudioUpgradeManagerImpl extends AbstractUpgradeManager<String> imp
 	@Override
 	public void upgradeExistingSites() throws UpgradeException {
 		upgrade();
+	}
+
+	@Override
+	public void upgrade() throws UpgradeException {
+		logger.info("Starting upgrade of all targets");
+		List<String> sites = getTargets();
+		AtomicBoolean upgradeFailed = new AtomicBoolean(false);
+		UpgradeException upgradeException = new UpgradeException("Failed to upgrade some sites");
+		try (ExecutorService taskExecutor = Executors.newFixedThreadPool(executorThreadCount)) {
+			for (String site : sites) {
+				taskExecutor.execute(() -> {
+					try {
+						upgrade(site);
+					} catch (UpgradeException e) {
+						logger.error("Failed to upgrade site '{}'", site, e);
+						upgradeException.addSuppressed(e);
+						upgradeFailed.set(true);
+					}
+				});
+			}
+			taskExecutor.shutdown();
+			taskExecutor.awaitTermination(executorTimeoutSeconds, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			logger.warn("Interrupted while waiting for site upgrades to complete, some sites may not be upgraded", e);
+			Thread.currentThread().interrupt(); // restore interrupt status
+			upgradeException.addSuppressed(e);
+			upgradeFailed.set(true);
+		}
+		if (upgradeFailed.get()) {
+			throw upgradeException;
+		}
 	}
 
 	protected boolean checkIfSiteRepoExists(String site) {
@@ -246,7 +287,7 @@ public class StudioUpgradeManagerImpl extends AbstractUpgradeManager<String> imp
 		String basePath = studioConfiguration.getProperty(CONFIGURATION_SITE_CONFIG_BASE_PATH_PATTERN);
 		String envPath = studioConfiguration.getProperty(CONFIGURATION_SITE_MUTLI_ENVIRONMENT_CONFIG_BASE_PATH_PATTERN);
 		Collection<RepositoryItem> modules = contentRepository.getContentChildren(site,
-			replace(basePath, Collections.singletonMap(CONFIG_KEY_MODULE, StringUtils.EMPTY), "{", "}"));
+				replace(basePath, Collections.singletonMap(CONFIG_KEY_MODULE, StringUtils.EMPTY), "{", "}"));
 		for (RepositoryItem module : modules) {
 			logger.debug("Look for configured publishing targets for module '{}' in site '{}'", module.name(), site);
 
@@ -255,7 +296,7 @@ public class StudioUpgradeManagerImpl extends AbstractUpgradeManager<String> imp
 			values.put(CONFIG_KEY_ENVIRONMENT, StringUtils.EMPTY);
 			try {
 				Collection<RepositoryItem> environments =
-					contentRepository.getContentChildren(site, replace(envPath, values, "{", "}"));
+						contentRepository.getContentChildren(site, replace(envPath, values, "{", "}"));
 
 				for (RepositoryItem env : environments) {
 					logger.debug("Add publishing target '{}' in site '{}'", env.name(), site);
