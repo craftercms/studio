bf9d2c488b0366c33480d95fa110cf1601b4b4cc
Jonathan Mendez <jonnathan.mendez@gmail.com>
2026-07-01 16:23:50 -0600

    Validate commit from in get history

--- src/main/java/org/craftercms/studio/api/v2/repository/GitContentRepository.java
+++ src/main/java/org/craftercms/studio/api/v2/repository/GitContentRepository.java
@@ -16,6 +16,17 @@
 
 package org.craftercms.studio.api.v2.repository;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.emptySet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.SequencedCollection;
+import java.util.Set;
+
 import org.craftercms.commons.git.utils.AuthenticationType;
 import org.craftercms.core.service.Item;
 import org.craftercms.studio.api.v1.constant.GitRepositories;
@@ -30,13 +41,6 @@ import org.craftercms.studio.model.history.ItemVersion;
 import org.craftercms.studio.model.history.RepositoryVersion;
 import org.springframework.core.io.Resource;
 import org.springframework.util.function.ThrowingConsumer;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.*;
-
-import static java.util.Collections.emptyList;
-import static java.util.Collections.emptySet;
 
 /**
  * Interface for content repositories that support git operations
@@ -200,8 +204,9 @@ public interface GitContentRepository extends ContentRepository {
 	 * @param limit      maximum number of versions to return
 	 * @return list of repository versions
 	 * @throws RepositoryException if there is any error reading the git log
+	 * @throws ServiceLayerException
 	 */
-	List<RepositoryVersion> getHistory(String siteId, String commitFrom, int limit) throws RepositoryException;
+	List<RepositoryVersion> getHistory(String siteId, String commitFrom, int limit) throws RepositoryException, ServiceLayerException;
 
 	/**
 	 * Get the new commits introduced by <code>commitId</code> into <code>baseCommit</code>.<br/>
